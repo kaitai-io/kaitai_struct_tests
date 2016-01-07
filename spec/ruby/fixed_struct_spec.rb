7d7af7fa58c302866546582c2e65063e71a5084f
Mikhail Yakshin <greycat@altlinux.org>
2016-01-08 02:01:17 +0300

    Test more types in fixed structure test

--- spec/ruby/fixed_struct_spec.rb
+++ spec/ruby/fixed_struct_spec.rb
@@ -13,5 +13,21 @@ RSpec.describe Fixed_structFormat do
     expect(r.header.sint16).to eq -1
     expect(r.header.sint32).to eq -1
     expect(r.header.sint64).to eq -1
+
+    expect(r.header.uint16le).to eq 66
+    expect(r.header.uint32le).to eq 66
+    expect(r.header.uint64le).to eq 66
+
+    expect(r.header.sint16le).to eq -66
+    expect(r.header.sint32le).to eq -66
+    expect(r.header.sint64le).to eq -66
+
+    expect(r.header.uint16be).to eq 66
+    expect(r.header.uint32be).to eq 66
+    expect(r.header.uint64be).to eq 66
+
+    expect(r.header.sint16be).to eq -66
+    expect(r.header.sint32be).to eq -66
+    expect(r.header.sint64be).to eq -66
   end
 end
