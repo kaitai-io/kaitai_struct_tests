8c8b222a2c9b51ecb716f8aa916bc0e4bffaed51
Mikhail Yakshin <greycat@altlinux.org>
2016-02-24 14:57:03 +0300

    Added more checks for _raw_* contents

--- spec/java/src/io/kaitai/struct/spec/SpecTests.java
+++ spec/java/src/io/kaitai/struct/spec/SpecTests.java
@@ -133,11 +133,25 @@ public class SpecTests {
         BufferedStruct r = BufferedStruct.fromFile(SRC_DIR + "buffered_struct.bin");
 
         assertEquals(r.len1(), 0x10);
+
+        assertEquals(r._raw_block1(), new byte[] {
+                0x42, 0, 0, 0,
+                0x43, 0, 0, 0,
+                -1, -1, -1, -1,
+                -1, -1, -1, -1,
+        });
         assertEquals(r.block1().number1(), 0x42);
         assertEquals(r.block1().number2(), 0x43);
+
         assertEquals(r.len2(), 0x8);
+
+        assertEquals(r._raw_block2(), new byte[] {
+                0x44, 0, 0, 0,
+                0x45, 0, 0, 0,
+        });
         assertEquals(r.block2().number1(), 0x44);
         assertEquals(r.block2().number2(), 0x45);
+
         assertEquals(r.finisher(), 0xee);
     }
 }
