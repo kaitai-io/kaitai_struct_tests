461c58fac71e08bfea113d6e5ff5905299e50257
Mikhail Yakshin <greycat@altlinux.org>
2016-02-26 03:07:06 +0300

    Ported repeat_eos_u4 to JavaScript and made it work

---
+++ spec/javascript/test_repeat_eos_u4.js
@@ -0,0 +1,6 @@
+var assert = require('assert');
+var testHelper = require('testHelper')
+
+testHelper('RepeatEosU4', 'src/repeat_eos_struct.bin', function(r) {
+    assert.deepEqual([0, 0x42, 0x42, 0x815], r.numbers)
+});
