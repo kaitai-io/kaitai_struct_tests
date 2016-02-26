b54fb06912429f92d09f11f66988584857b8dc24
Mikhail Yakshin <greycat@altlinux.org>
2016-02-26 11:01:50 +0300

    Fixed the test for proper values

--- spec/javascript/test_repeat_n_struct.js
+++ spec/javascript/test_repeat_n_struct.js
@@ -3,8 +3,8 @@ var testHelper = require('testHelper');
 
 testHelper('RepeatNStruct', 'src/repeat_n_struct.bin', function(r) {
     assert.equal(r.chunks.length, 2);
-    assert.equal(r.chunks[0].offset, 0);
-    assert.equal(r.chunks[0].len, 0x42);
-    assert.equal(r.chunks[1].offset, 0x42);
-    assert.equal(r.chunks[1].len, 0x815);
+    assert.equal(r.chunks[0].offset, 0x10)
+    assert.equal(r.chunks[0].len, 0x2078)
+    assert.equal(r.chunks[1].offset, 0x2088)
+    assert.equal(r.chunks[1].len, 0xf)
 });
