399da0305ef7a8506a6e87f237fba63dee215b5d
Mikhail Yakshin <greycat@altlinux.org>
2016-03-04 10:44:41 +0300

    Ruby: nav_root: fixed member style naming issues

--- spec/ruby/nav_root_spec.rb
+++ spec/ruby/nav_root_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe NavRoot do
   it 'parses test properly' do
     r = NavRoot.from_file('src/nav.bin')
 
-    expect(r.header.qtyEntries).to eq 2
-    expect(r.header.filenameLen).to eq 8
+    expect(r.header.qty_entries).to eq 2
+    expect(r.header.filename_len).to eq 8
 
     expect(r.index.entries.size).to eq 2
     expect(r.index.entries[0].filename).to eq 'FIRST___'
