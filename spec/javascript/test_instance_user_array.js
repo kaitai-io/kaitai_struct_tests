ec1a67485a2ca1d734f33f96c7b0dcd754eb307f
Mikhail Yakshin <greycat@altlinux.org>
2016-03-09 10:42:39 +0300

    Ported "instance_user_array" to JS

---
+++ spec/javascript/test_instance_user_array.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var testHelper = require('testHelper');
+
+testHelper('InstanceUserArray', 'src/instance_std_array.bin', function(r) {
+    assert.equal(r.ofs, 0x10);
+    assert.equal(r.qtyEntries, 3);
+    assert.equal(r.entrySize, 4);
+
+    assert.equal(r.entries[0].word1, 0x1111);
+    assert.equal(r.entries[0].word2, 0x1111);
+    assert.equal(r.entries[1].word1, 0x2222);
+    assert.equal(r.entries[1].word2, 0x2222);
+    assert.equal(r.entries[2].word1, 0x3333);
+    assert.equal(r.entries[2].word2, 0x3333);
+});
