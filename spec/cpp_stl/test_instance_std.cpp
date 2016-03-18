b5f4a4dcf3c34087ab1664ec8331aa1320299ae5
Mikhail Yakshin <greycat@altlinux.org>
2016-03-18 14:45:42 +0300

    C++: ported "instance_std"

---
+++ spec/cpp_stl/test_instance_std.cpp
@@ -0,0 +1,16 @@
+#include <boost/test/unit_test.hpp>
+
+#include <instance_std.h>
+
+#include <iostream>
+#include <fstream>
+
+BOOST_AUTO_TEST_CASE(test_instance_std) {
+    std::ifstream ifs("src/str_encodings.bin", std::ifstream::binary);
+    kaitai::kstream ks(&ifs);
+    instance_std* r = new instance_std(&ks);
+
+    BOOST_CHECK_EQUAL(r->header(), "Some ");
+
+    delete r;
+}
