221d9e4c054422d31060854601dbe0df0dbd1fb3
Mikhail Yakshin <greycat@altlinux.org>
2016-03-18 11:02:33 +0300

    C++/STL specs: added distinct main, ported "fixed_struct"

---
+++ spec/cpp_stl/main.cpp
@@ -0,0 +1,7 @@
+#define BOOST_TEST_MAIN
+#if !defined( WIN32 )
+    #define BOOST_TEST_DYN_LINK
+#endif
+#include <boost/test/included/unit_test.hpp>
+
+#define BOOST_TEST_MODULE "C++/STL test units for Kaitai Struct"
--- spec/cpp_stl/test_hello_world.cpp
+++ spec/cpp_stl/test_hello_world.cpp
@@ -1,22 +1,16 @@
-#define BOOST_TEST_MAIN
-#if !defined( WIN32 )
-    #define BOOST_TEST_DYN_LINK
-#endif
 #include <boost/test/unit_test.hpp>
 
-#include <HelloWorld.h>
-
-// #define BOOST_TEST_MODULE HelloWorld
+#include <hello_world.h>
 
 #include <iostream>
 #include <fstream>
 
-BOOST_AUTO_TEST_CASE(my_test) {
-//    HelloWorld* r = HelloWorld::fromFile("src/fixed_struct.bin");
+BOOST_AUTO_TEST_CASE(test_hello_world) {
+//    hello_world* r = hello_world::from_file("src/fixed_struct.bin");
 
     std::ifstream ifs("src/fixed_struct.bin", std::ifstream::binary);
-    KaitaiStream ks(&ifs);
-    HelloWorld* r = new HelloWorld(&ks);
+    kaitai::kstream ks(&ifs);
+    hello_world* r = new hello_world(&ks);
 
     BOOST_CHECK_EQUAL(r->one(), 0x50);
 
