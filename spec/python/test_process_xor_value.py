15bee290910152042bd4a03800f52ab16c6f5fb0
Mikhail Yakshin <greycat@altlinux.org>
2016-03-23 22:58:35 +0300

    Fixed wrong test name

--- spec/python/test_process_xor_value.py
+++ spec/python/test_process_xor_value.py
@@ -3,7 +3,7 @@ import unittest
 from process_xor_value import ProcessXorValue
 
 class TestProcessXorValue(unittest.TestCase):
-    def test_process_xor_const(self):
+    def test_process_xor_value(self):
         r = ProcessXorValue.from_file("src/process_xor_1.bin")
 
         self.assertEquals(r.key, 0xff)
