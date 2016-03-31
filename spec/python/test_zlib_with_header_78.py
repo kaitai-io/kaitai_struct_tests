4d457b849d02754a022a2824e44d8e49cfac154c
Mikhail Yakshin <greycat@altlinux.org>
2016-03-31 09:47:24 +0300

    zlib_with_header_78: port to Python

---
+++ spec/python/test_zlib_with_header_78.py
@@ -0,0 +1,9 @@
+import unittest
+
+from zlib_with_header_78 import ZlibWithHeader78
+
+class TestZlibWithHeader78(unittest.TestCase):
+    def test_zlib_with_header_78(self):
+        r = ZlibWithHeader78.from_file("src/zlib_with_header_78.bin")
+
+        self.assertEquals(r.data, "a quick brown fox jumps over")
