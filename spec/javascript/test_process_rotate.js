3919c888acd82b2903e46edbddbeb3f734829ca3
Mikhail Yakshin <greycat@altlinux.org>
2016-04-19 14:21:01 +0300

    JavaScript: added "process_rotate" spec

---
+++ spec/javascript/test_process_rotate.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var testHelper = require('testHelper');
+
+testHelper('ProcessRotate', 'src/process_rotate.bin', function(r) {
+    assert.equal(KaitaiStream.arrayToString(r.buf1, "UTF-8"), "Hello");
+    assert.equal(KaitaiStream.arrayToString(r.buf2, "UTF-8"), "World");
+    assert.equal(KaitaiStream.arrayToString(r.buf3, "UTF-8"), "There");
+});
