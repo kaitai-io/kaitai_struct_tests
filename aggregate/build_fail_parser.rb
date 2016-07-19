bb660f60486ce984aba617a844b08240d087b981
Mikhail Yakshin <greycat@altlinux.org>
2016-07-19 10:57:31 +0300

    Added aggregation of build fail results

---
+++ aggregate/build_fail_parser.rb
@@ -0,0 +1,17 @@
+class BuildFailParser
+  def initialize(dir)
+    @dir = dir
+  end
+
+  def each_test
+    File.open("#{@dir}/build.fails", 'r') { |f|
+      f.each_line { |l|
+        case l
+        when /^(.*?)\.java$/
+          name = $1
+          yield TestResult.new(name, :format_build_failed, 0, nil)
+        end
+      }
+    }
+  end
+end
