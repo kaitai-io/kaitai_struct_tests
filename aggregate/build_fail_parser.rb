9b5fc3ccc55fad049f86bf7be837b049db6a9208
Mikhail Yakshin <greycat@altlinux.org>
2016-07-19 12:11:52 +0300

    aggregate: proper aggregation of format build failed vs spec build failed results

--- aggregate/build_fail_parser.rb
+++ aggregate/build_fail_parser.rb
@@ -7,6 +7,9 @@ class BuildFailParser
     File.open("#{@dir}/build.fails", 'r') { |f|
       f.each_line { |l|
         case l
+        when /^Test(.*?)\.java$/
+          name = $1
+          yield TestResult.new(name, :spec_build_failed, 0, nil)
         when /^(.*?)\.java$/
           name = $1
           yield TestResult.new(name, :format_build_failed, 0, nil)
