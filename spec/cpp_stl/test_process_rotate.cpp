fa5e4e58c9cd5f7d460c29d1cb71ea23bb7bab9b
Mikhail Yakshin <greycat@altlinux.org>
2016-07-24 14:56:25 +0300

    process_rotate: ported to C++/STL

---
+++ spec/cpp_stl/test_process_rotate.cpp
@@ -0,0 +1,17 @@
+#include <boost/test/unit_test.hpp>
+
+#include <process_rotate.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+BOOST_AUTO_TEST_CASE(test_process_rotate) {
+    std::ifstream ifs("src/process_rotate.bin", std::ifstream::binary);
+    kaitai::kstream ks(&ifs);
+    process_rotate_t* r = new process_rotate_t(&ks);
+
+    BOOST_CHECK_EQUAL(r->buf1(), "Hello");
+    BOOST_CHECK_EQUAL(r->buf2(), "World");
+    BOOST_CHECK_EQUAL(r->buf3(), "There");
+}
