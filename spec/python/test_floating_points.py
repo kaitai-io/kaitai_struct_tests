500968a4460ebb815b52cfbba092b1b4c6e06378
Mikhail Yakshin <greycat@altlinux.org>
2016-07-26 16:55:35 +0300

    spec/python/test_floating_points: introduced adjustable precision (places)

--- spec/python/test_floating_points.py
+++ spec/python/test_floating_points.py
@@ -6,11 +6,13 @@ class TestFloatingPoints(unittest.TestCase):
     def test_floating_points(self):
         r = FloatingPoints.from_file("src/floating_points.bin")
 
-        self.assertAlmostEqual(r.single_value, 1.2345)
-        self.assertAlmostEqual(r.single_value_be, 1.2345)
-        self.assertAlmostEqual(r.double_value, 123.456)
-        self.assertAlmostEqual(r.double_value_be, 123.456)
+        prec = 6
 
-        self.assertAlmostEqual(r.single_value_plus_int, 2.2345)
-        self.assertAlmostEqual(r.single_value_plus_float, 1.7345)
-        self.assertAlmostEqual(r.double_value_plus_float, 123.506)
+        self.assertAlmostEqual(r.single_value, 1.2345, prec)
+        self.assertAlmostEqual(r.single_value_be, 1.2345, prec)
+        self.assertAlmostEqual(r.double_value, 123.456, prec)
+        self.assertAlmostEqual(r.double_value_be, 123.456, prec)
+
+        self.assertAlmostEqual(r.single_value_plus_int, 2.2345, prec)
+        self.assertAlmostEqual(r.single_value_plus_float, 1.7345, prec)
+        self.assertAlmostEqual(r.double_value_plus_float, 123.506, prec)
