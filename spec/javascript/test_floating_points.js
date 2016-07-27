b905362ac542d81f37f27f6e59cb322e8c1ef376
Mikhail Yakshin <greycat@altlinux.org>
2016-07-27 12:11:46 +0300

    floating_points: fixed JavaScript spec to match new binary content

--- spec/javascript/test_floating_points.js
+++ spec/javascript/test_floating_points.js
@@ -4,12 +4,14 @@ var testHelper = require('testHelper');
 testHelper('FloatingPoints', 'src/floating_points.bin', function(r) {
     var delta = 1e-6;
     
-    assert(Math.abs(r.singleValue - 1.2345) < delta);
-    assert(Math.abs(r.singleValueBe - 1.2345) < delta);
-    assert(Math.abs(r.doubleValue - 123.456) < delta);
-    assert(Math.abs(r.doubleValueBe - 123.456) < delta);
-    
-    assert(Math.abs(r.singleValuePlusInt - 2.2345) < delta);
-    assert(Math.abs(r.singleValuePlusFloat - 1.7345) < delta);
-    assert(Math.abs(r.doubleValuePlusFloat - 123.506) < delta);
+    assert.equal(r.singleValue, 0.5);
+    assert.equal(r.singleValueBe, 0.5);
+    assert.equal(r.doubleValue, 0.25);
+    assert.equal(r.doubleValueBe, 0.25);
+
+    assert(Math.abs(r.approximateValue - 1.2345) < delta);
+
+    assert(Math.abs(r.singleValuePlusInt - 1.5) < delta);
+    assert(Math.abs(r.singleValuePlusFloat - 1.0) < delta);
+    assert(Math.abs(r.doubleValuePlusFloat - 0.3) < delta);
 });
