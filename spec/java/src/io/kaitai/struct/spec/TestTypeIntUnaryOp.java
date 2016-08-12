1557ac6dd12d6a7a00c1c6f31f5a7b252270f812
Mikhail Yakshin <greycat@altlinux.org>
2016-08-12 15:25:47 +0300

    Added type_int_unary_op + Java spec

---
+++ spec/java/src/io/kaitai/struct/spec/TestTypeIntUnaryOp.java
@@ -0,0 +1,21 @@
+package io.kaitai.struct.spec;
+
+import io.kaitai.struct.testformats.TypeIntUnaryOp;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class TestTypeIntUnaryOp extends CommonSpec {
+    @Test
+    public void testTypeIntUnaryOp() throws Exception {
+        TypeIntUnaryOp r = TypeIntUnaryOp.fromFile(SRC_DIR + "fixed_struct.bin");
+
+        assertEquals(r.valueS2(), (short) 0x4150);
+        assertEquals(r.valueS8(), 0x4150ffff312d4b43L);
+        assertTrue(r.unaryS2() instanceof Integer);
+        assertTrue(r.unaryS8() instanceof Long);
+        assertEquals(r.unaryS2().shortValue(), (short) -0x4150);
+        assertEquals(r.unaryS8().longValue(), -0x4150ffff312d4b43L);
+    }
+}
