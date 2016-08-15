c3741e0d5620b8cff97eaed9679fbb6d6619c36d
markbook2 <morpho-os@users.noreply.github.com>
2016-08-15 11:48:50 +0300

    Fixed HelloWorld PHP test

--- spec/php/HelloWorldTest.php
+++ spec/php/HelloWorldTest.php
@@ -1,15 +1,17 @@
 <?php
-namespace Kaitai\Tests;
+namespace Kaitai\Struct\Tests;
+
+use Kaitai\Struct\Tests\HelloWorld;
 
 class HelloWorldTest extends \PHPUnit_Framework_TestCase {
     const FILE_PATH = __DIR__ . '/../../src/fixed_struct.bin';
     public function testAccessProperty_ThroughDirectCall() {
-        $r = \HelloWorld::fromFile(self::FILE_PATH);
+        $r = HelloWorld::fromFile(self::FILE_PATH);
         $this->assertEquals(0x50, $r->one());
     }
 
     public function testAccessProperty_ThroughGetCall() {
-        $r = \HelloWorld::fromFile(self::FILE_PATH);
+        $r = HelloWorld::fromFile(self::FILE_PATH);
         $this->assertEquals(0x50, $r->one);
     }
 }
