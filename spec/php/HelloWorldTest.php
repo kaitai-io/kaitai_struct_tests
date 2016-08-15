83da13f0da08b2828fb1aaf129a21ca600b923e1
Mikhail Yakshin <greycat@altlinux.org>
2016-08-15 21:45:43 +0300

    spec/php/HelloWorldTest: fixed testcase so it would adhere to 1 ksy = 1 test case = 1 test method

--- spec/php/HelloWorldTest.php
+++ spec/php/HelloWorldTest.php
@@ -5,13 +5,11 @@ use Kaitai\Struct\Tests\HelloWorld;
 
 class HelloWorldTest extends \PHPUnit_Framework_TestCase {
     const FILE_PATH = __DIR__ . '/../../src/fixed_struct.bin';
-    public function testAccessProperty_ThroughDirectCall() {
+    public function testHelloWorld() {
         $r = HelloWorld::fromFile(self::FILE_PATH);
+        // Access property through direct call
         $this->assertEquals(0x50, $r->one());
-    }
-
-    public function testAccessProperty_ThroughGetCall() {
-        $r = HelloWorld::fromFile(self::FILE_PATH);
+        // Access property through get call
         $this->assertEquals(0x50, $r->one);
     }
 }
