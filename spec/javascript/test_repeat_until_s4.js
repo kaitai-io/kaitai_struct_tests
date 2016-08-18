61a464c713364c8af5ab0a4433a38a825a17c195
Mikhail Yakshin <greycat@altlinux.org>
2016-08-19 00:45:40 +0300

    spec/javascript/test_repeat_until_s4.js: fixed array comparison

--- spec/javascript/test_repeat_until_s4.js
+++ spec/javascript/test_repeat_until_s4.js
@@ -2,6 +2,6 @@ var assert = require('assert');
 var testHelper = require('testHelper');
 
 testHelper('RepeatUntilS4', 'src/repeat_until_s4.bin', function(r) {
-  assert.equal(r.entries, [0x42, 0x1337, -251658241, -1]);
+  assert.deepEqual(r.entries, [0x42, 0x1337, -251658241, -1]);
   assert.equal(r.afterall, "foobar");
 });
