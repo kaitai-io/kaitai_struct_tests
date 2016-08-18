900220a567c46df36c9bfd1d8524d4fb00f3d913
Mikhail Yakshin <greycat@altlinux.org>
2016-08-18 22:07:20 +0300

    Ported repeat_until_s4 for Python

---
+++ spec/python/test_repeat_until_s4.py
@@ -0,0 +1,10 @@
+import unittest
+
+from repeat_until_s4 import RepeatUntilS4
+
+class TestRepeatUntilS4(unittest.TestCase):
+    def test_repeat_until_s4(self):
+        r = RepeatUntilS4.from_file("src/repeat_until_s4.bin")
+
+        self.assertEquals(r.entries, [0x42, 0x1337, -251658241, -1])
+        self.assertEquals(r.afterall, "foobar")
