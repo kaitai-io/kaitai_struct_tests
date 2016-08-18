e2fd2706da9002dac489db0bc75c6ad0a895cbd7
Mikhail Yakshin <greycat@altlinux.org>
2016-08-19 00:49:51 +0300

    type_int_unary_op: relaxed equality assertions

--- spec/javascript/test_type_int_unary_op.js
+++ spec/javascript/test_type_int_unary_op.js
@@ -3,7 +3,10 @@ var testHelper = require('testHelper');
 
 testHelper('TypeIntUnaryOp', 'src/fixed_struct.bin', function(r) {
   assert.equal(r.valueS2, 0x4150);
-  assert.equal(r.valueS8, 0x4150ffff312d4b43);
+  // approximate equality
+  assert.equal(r.valueS8 >> 8, 0x4150ffff312d4b43 >> 8);
+
   assert.equal(r.unaryS2, -0x4150);
-  assert.equal(r.unaryS8, -0x4150ffff312d4b43);
+  // approximate equality
+  assert.equal(r.unaryS8 >> 8, -0x4150ffff312d4b43 >> 8);
 });
