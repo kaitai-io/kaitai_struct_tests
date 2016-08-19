ebdfad1f1c8446ad81fadcaf85e5b412956bbb2d
Mikhail Yakshin <greycat@altlinux.org>
2016-08-19 18:45:06 +0300

    Ported RepeatUntilS4 to PHP

---
+++ spec/php/RepeatUntilS4Test.php
@@ -0,0 +1,11 @@
+<?php
+namespace Kaitai\Struct\Tests;
+
+class RepeatUntilS4Test extends TestCase {
+    public function testRepeatUntilS4() {
+        $r = RepeatUntilS4::fromFile(self::SRC_DIR_PATH . "/repeat_n_struct.bin");
+
+        $this->assertEquals([0x42, 0x1337, -251658241, -1], $r->entries);
+        $this->assertEquals("foobar", $r->afterall);
+    }
+}
