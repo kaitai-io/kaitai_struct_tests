b263651560d4df350e897bde9d737f8d0bf181e5
Mikhail Yakshin <greycat@altlinux.org>
2016-08-24 10:56:32 +0300

    php: fixed RepeatEosU4 test

--- spec/php/RepeatEosU4Test.php
+++ spec/php/RepeatEosU4Test.php
@@ -3,11 +3,7 @@ namespace Kaitai\Struct\Tests;
 
 class RepeatEosU4Test extends TestCase {
     public function testRepeatEosU4() {
-        $this->markTestIncomplete();
-/*
-        RepeatEosU4 r = RepeatEosU4::fromFile(self::SRC_DIR_PATH . "repeat_eos_struct.bin");
-
-        $this->assertEquals(r.numbers().toArray(), new long[] { 0, 0x42, 0x42, 0x815 });
-*/    
+        $r = RepeatEosU4::fromFile(self::SRC_DIR_PATH . "/repeat_eos_struct.bin");
+        $this->assertEquals([0, 0x42, 0x42, 0x815], $r->numbers);
     }
 }
