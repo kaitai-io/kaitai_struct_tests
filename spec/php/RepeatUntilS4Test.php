f414c30e23065ee52d981a7bcd299ef41c8c962c
markbook2 <morpho-os@users.noreply.github.com>
2016-08-24 03:33:23 +0300

    Fixed the RepeatUntilS4Test

--- spec/php/RepeatUntilS4Test.php
+++ spec/php/RepeatUntilS4Test.php
@@ -3,7 +3,7 @@ namespace Kaitai\Struct\Tests;
 
 class RepeatUntilS4Test extends TestCase {
     public function testRepeatUntilS4() {
-        $r = RepeatUntilS4::fromFile(self::SRC_DIR_PATH . "/repeat_n_struct.bin");
+        $r = RepeatUntilS4::fromFile(self::SRC_DIR_PATH . "/repeat_until_s4.bin");
 
         $this->assertEquals([0x42, 0x1337, -251658241, -1], $r->entries);
         $this->assertEquals("foobar", $r->afterall);
