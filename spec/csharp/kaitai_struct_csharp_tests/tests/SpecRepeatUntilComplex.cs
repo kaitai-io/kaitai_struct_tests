52321b4c00ba66e60cc0793fc2440ab399cc50d6
Daniel Walder <daniel.walder@gmail.com>
2016-08-25 19:17:29 +1000

    Add repeat_until_complex test

---
+++ spec/csharp/kaitai_struct_csharp_tests/tests/SpecRepeatUntilComplex.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+
+namespace Kaitai
+{
+    [TestFixture]
+    public class SpecRepeatUntilComplex : CommonSpec
+    {
+        [Test]
+        public void TestRepeatUntilComplex()
+        {
+            var r = RepeatUntilComplex.FromFile(SourceFile("repeat_until_complex.bin"));
+            Assert.AreEqual(r.First.Count, 3);
+            Assert.AreEqual(r.First[0].Count, 4);
+            Assert.AreEqual(r.First[0].Values, new int[] { 1, 2, 3, 4 });
+            Assert.AreEqual(r.First[1].Count, 2);
+            Assert.AreEqual(r.First[1].Values, new int[] { 1, 2 });
+            Assert.AreEqual(r.First[2].Count, 0);
+            Assert.AreEqual(r.First[2].Values, new int[] {  });
+    
+            Assert.AreEqual(r.Second.Count, 4);
+            Assert.AreEqual(r.Second[0].Count, 6);
+            Assert.AreEqual(r.Second[0].Values, new int[] { 1, 2, 3, 4, 5, 6 });
+            Assert.AreEqual(r.Second[1].Count, 3);
+            Assert.AreEqual(r.Second[1].Values, new int[] { 1, 2, 3 });
+            Assert.AreEqual(r.Second[2].Count, 4);
+            Assert.AreEqual(r.Second[2].Values, new int[] { 1, 2, 3, 4 });
+            Assert.AreEqual(r.Second[3].Count, 0);
+            Assert.AreEqual(r.Second[3].Values, new int[] {  });
+            
+            Assert.AreEqual(r.Third, new int[] { 102, 111, 111, 98, 97, 114, 0 });
+        }
+    }
+}
