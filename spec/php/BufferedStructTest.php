a9d00c94761a3a2b3cf3b5758266f11177b304fd
markbook2 <morpho-os@users.noreply.github.com>
2016-08-25 21:37:03 +0300

    Fixed BufferedStructTest

--- spec/php/BufferedStructTest.php
+++ spec/php/BufferedStructTest.php
@@ -3,31 +3,28 @@ namespace Kaitai\Struct\Tests;
 
 class BufferedStructTest extends TestCase {
     public function testBufferedStruct() {
-        $this->markTestIncomplete();
-/*
-        BufferedStruct r = BufferedStruct::fromFile(self::SRC_DIR_PATH . "buffered_struct.bin");
+        $r = BufferedStruct::fromFile(self::SRC_DIR_PATH . "/buffered_struct.bin");
 
-        $this->assertEquals(r.len1(), 0x10);
+        $this->assertEquals($r->len1(), 0x10);
 
-        $this->assertEquals(r._raw_block1(), new byte[] {
-                0x42, 0, 0, 0,
-                0x43, 0, 0, 0,
-                -1, -1, -1, -1,
-                -1, -1, -1, -1,
-        });
-        $this->assertEquals(r.block1().number1(), 0x42);
-        $this->assertEquals(r.block1().number2(), 0x43);
+        $expectedBytes = "\x42\x00\x00\x00"
+            . "\x43\x00\x00\x00"
+            . "\xff\xff\xff\xff"
+            . "\xff\xff\xff\xff";
+        $this->assertEquals($expectedBytes, $r->_raw_block1());
+        $this->assertEquals($r->block1()->number1(), 0x42);
+        $this->assertEquals($r->block1()->number2(), 0x43);
 
-        $this->assertEquals(r.len2(), 0x8);
+        $this->assertEquals($r->len2(), 0x8);
 
-        $this->assertEquals(r._raw_block2(), new byte[] {
-                0x44, 0, 0, 0,
-                0x45, 0, 0, 0,
-        });
-        $this->assertEquals(r.block2().number1(), 0x44);
-        $this->assertEquals(r.block2().number2(), 0x45);
+        $this->assertEquals(
+            "\x44\x00\x00\x00"
+            . "\x45\x00\x00\x00",
+            $r->_raw_block2()
+        );
+        $this->assertEquals($r->block2()->number1(), 0x44);
+        $this->assertEquals($r->block2()->number2(), 0x45);
 
-        $this->assertEquals(r.finisher(), 0xee);
-*/    
+        $this->assertEquals($r->finisher(), 0xee);
     }
 }
