a679f3d822724a7918ab21dee2da1b3ad37ed371
Mikhail Yakshin <greycat@altlinux.org>
2016-08-25 17:36:34 +0300

    Simplified C++ repeat_until_s4 test using helper

--- spec/cpp_stl/test_repeat_until_s4.cpp
+++ spec/cpp_stl/test_repeat_until_s4.cpp
@@ -6,21 +6,14 @@
 #include <fstream>
 #include <vector>
 
+#include "helpers.h"
+
 BOOST_AUTO_TEST_CASE(test_repeat_until_s4) {
     std::ifstream ifs("src/repeat_until_s4.bin", std::ifstream::binary);
     kaitai::kstream ks(&ifs);
     repeat_until_s4_t* r = new repeat_until_s4_t(&ks);
 
-    std::vector<int32_t> exp_vector;
-    exp_vector.push_back(0x42);
-    exp_vector.push_back(0x1337);
-    exp_vector.push_back(-251658241);
-    exp_vector.push_back(-1);
-
-    BOOST_CHECK_EQUAL_COLLECTIONS(
-        r->entries()->begin(), r->entries()->end(),
-        exp_vector.begin(), exp_vector.end()
-    );
+    COMPARE_ARRAY(uint32_t, r->entries(), 0x42, 0x1337, -251658241, -1);
     BOOST_CHECK_EQUAL(r->afterall(), std::string("foobar"));
 
     delete r;
