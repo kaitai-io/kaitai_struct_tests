bb1663c20640c8db27fee91b77636ad3a675c9cb
Mikhail Yakshin <greycat@altlinux.org>
2016-08-25 14:10:26 +0300

    JavaScript: fixed instance_std_array that was broken for ages; you can't just compare Uint8Arrays with deepEqual in JS :(

--- spec/javascript/test_instance_std_array.js
+++ spec/javascript/test_instance_std_array.js
@@ -2,13 +2,12 @@ var assert = require('assert');
 var testHelper = require('testHelper');
 
 testHelper('InstanceStdArray', 'src/instance_std_array.bin', function(r) {
-    assert.equal(r.ofs, 0x10)
-    assert.equal(r.qtyEntries, 3)
-    assert.equal(r.entrySize, 4)
+    assert.equal(r.ofs, 0x10);
+    assert.equal(r.qtyEntries, 3);
+    assert.equal(r.entrySize, 4);
 
-    assert.deepEqual(r.entries, [
-        new Uint8Array([0x11, 0x11, 0x11, 0x11]),
-        new Uint8Array([0x22, 0x22, 0x22, 0x22]),
-        new Uint8Array([0x33, 0x33, 0x33, 0x33]),
-    ])
+    assert.equal(r.entries.length, 3);
+    assert.equal(r.entries[0].toString(), new Uint8Array([0x11, 0x11, 0x11, 0x11]).toString());
+    assert.equal(r.entries[1].toString(), new Uint8Array([0x22, 0x22, 0x22, 0x22]).toString());
+    assert.equal(r.entries[2].toString(), new Uint8Array([0x33, 0x33, 0x33, 0x33]).toString());
 });
