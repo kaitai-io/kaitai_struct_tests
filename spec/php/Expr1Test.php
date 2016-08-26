47db0b45a34e36001c114a2658ff05d93ebb3936
markbook2 <morpho-os@users.noreply.github.com>
2016-08-26 17:50:36 +0300

    Fixed Expr1Test

--- spec/php/Expr1Test.php
+++ spec/php/Expr1Test.php
@@ -3,14 +3,11 @@ namespace Kaitai\Struct\Tests;
 
 class Expr1Test extends TestCase {
     public function testExpr1() {
-        $this->markTestIncomplete();
-/*
-        Expr1 r = Expr1::fromFile(self::SRC_DIR_PATH . "str_encodings.bin");
+        $r = Expr1::fromFile(self::SRC_DIR_PATH . "/str_encodings.bin");
 
-        $this->assertEquals(r.lenOf1(), 10);
-        $this->assertEquals(r.lenOf1Mod().intValue(), 8);
-        $this->assertEquals(r.str1(), "Some ASC");
-        $this->assertEquals(r.str1Len().intValue(), 8);
-*/    
+        $this->assertEquals($r->lenOf1(), 10);
+        $this->assertEquals($r->lenOf1Mod(), 8);
+        $this->assertEquals($r->str1(), "Some ASC");
+        $this->assertEquals($r->str1Len(), 8);
     }
 }
