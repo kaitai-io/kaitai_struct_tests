f712ee2aef8eb582c1bcb7370cd5b78aa4e78ffd
markbook2 <morpho-os@users.noreply.github.com>
2016-08-29 22:34:53 +0300

    Fixed EnumIfTest

--- spec/php/EnumIfTest.php
+++ spec/php/EnumIfTest.php
@@ -3,19 +3,16 @@ namespace Kaitai\Struct\Tests;
 
 class EnumIfTest extends TestCase {
     public function testEnumIf() {
-        $this->markTestIncomplete();
-/*
-        EnumIf r = EnumIf::fromFile(self::SRC_DIR_PATH . "if_struct.bin");
+        $r = EnumIf::fromFile(self::SRC_DIR_PATH . "/if_struct.bin");
 
-        $this->assertEquals(r.op1().opcode(), EnumIf.Opcodes.A_STRING);
-        $this->assertEquals(r.op1().argStr().str(), "foo");
+        $this->assertEquals($r->op1()->opcode(), EnumIf::Opcodes()::A_STRING);
+        $this->assertEquals("foo", $r->op1()->argStr()->str());
 
-        $this->assertEquals(r.op2().opcode(), EnumIf.Opcodes.A_TUPLE);
-        $this->assertEquals(r.op2().argTuple().num1(), 0x42);
-        $this->assertEquals(r.op2().argTuple().num2(), 0x43);
+        $this->assertEquals($r->op2()->opcode(), EnumIf::Opcodes()::A_TUPLE);
+        $this->assertEquals(0x42, $r->op2()->argTuple()->num1());
+        $this->assertEquals(0x43, $r->op2()->argTuple()->num2());
 
-        $this->assertEquals(r.op3().opcode(), EnumIf.Opcodes.A_STRING);
-        $this->assertEquals(r.op3().argStr().str(), "bar");
-*/    
+        $this->assertEquals($r->op3()->opcode(), EnumIf::Opcodes()::A_STRING);
+        $this->assertEquals("bar", $r->op3()->argStr()->str());
     }
 }
