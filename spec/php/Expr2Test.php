20525f2f3522bd14fa6b9c8868a0e5be5e2ab85f
markbook2 <morpho-os@users.noreply.github.com>
2016-08-29 22:34:11 +0300

    Fixed Expr2Test

--- spec/php/Expr2Test.php
+++ spec/php/Expr2Test.php
@@ -3,29 +3,26 @@ namespace Kaitai\Struct\Tests;
 
 class Expr2Test extends TestCase {
     public function testExpr2() {
-        $this->markTestIncomplete();
-/*
-        Expr2 r = Expr2::fromFile(self::SRC_DIR_PATH . "str_encodings.bin");
+        $r = Expr2::fromFile(self::SRC_DIR_PATH . "/str_encodings.bin");
 
-        $this->assertEquals(r.str1().lenOrig(), 10);
-        $this->assertEquals(r.str1().lenMod().intValue(), 7);
-        $this->assertEquals(r.str1().str(), "Some AS");
+        $this->assertEquals(10, $r->str1()->lenOrig());
+        $this->assertEquals(7, $r->str1()->lenMod());
+        $this->assertEquals("Some AS", $r->str1()->str());
 
-        $this->assertEquals(r.str1Len().intValue(), 7);
-        $this->assertEquals(r.str1LenMod().intValue(), 7);
-        $this->assertEquals(r.str1Byte1().intValue(), 0x49);
-        $this->assertEquals(r.str1Avg().intValue(), 0x49);
-        $this->assertEquals(r.str1Char5(), "e");
+        $this->assertEquals(7, $r->str1Len());
+        $this->assertEquals(7, $r->str1LenMod());
+        $this->assertEquals(0x49, $r->str1Byte1());
+        $this->assertEquals(0x49, $r->str1Avg());
+        $this->assertEquals("e", $r->str1Char5());
 
-        $this->assertEquals(r.str1Tuple5().byte0(), 0x65);
-        $this->assertEquals(r.str1Tuple5().byte1(), 0x20);
-        $this->assertEquals(r.str1Tuple5().byte2(), 0x41);
-        $this->assertEquals(r.str1Tuple5().avg().intValue(), 0x30);
+        $this->assertEquals(0x65, $r->str1Tuple5()->byte0());
+        $this->assertEquals(0x20, $r->str1Tuple5()->byte1());
+        $this->assertEquals(0x41, $r->str1Tuple5()->byte2());
+        $this->assertEquals(0x30, $r->str1Tuple5()->avg());
 
-        $this->assertEquals(r.str2Tuple5().byte0(), 0x65);
-        $this->assertEquals(r.str2Tuple5().byte1(), 0x20);
-        $this->assertEquals(r.str2Tuple5().byte2(), 0x41);
-        $this->assertEquals(r.str2Tuple5().avg().intValue(), 0x30);
-*/    
+        $this->assertEquals(0x65, $r->str2Tuple5()->byte0());
+        $this->assertEquals(0x20, $r->str2Tuple5()->byte1());
+        $this->assertEquals(0x41, $r->str2Tuple5()->byte2());
+        $this->assertEquals(0x30, $r->str2Tuple5()->avg());
     }
 }
