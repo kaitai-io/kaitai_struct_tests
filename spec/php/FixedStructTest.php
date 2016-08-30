7641d2a9174e12f6d33f4d5fd925f566ccaf40c9
markbook2 <morpho-os@users.noreply.github.com>
2016-08-30 03:43:09 +0300

    Fixed FixedStructTest

--- spec/php/FixedStructTest.php
+++ spec/php/FixedStructTest.php
@@ -3,36 +3,35 @@ namespace Kaitai\Struct\Tests;
 
 class FixedStructTest extends TestCase {
     public function testFixedStruct() {
-        $this->markTestIncomplete();
-/*
-        FixedStruct r = FixedStruct::fromFile(self::SRC_DIR_PATH . "fixed_struct.bin");
+        $r = FixedStruct::fromFile(self::SRC_DIR_PATH . "/fixed_struct.bin");
 
-        $this->assertEquals(r.hdr().uint8(), 255);
-        $this->assertEquals(r.hdr().uint16(), 65535);
-        $this->assertEquals(r.hdr().uint32(), 4294967295L);
-        //$this->assertEquals(r.hdr().uint64(), 18446744073709551615);
-        $this->assertEquals(r.hdr().uint64(), 0xFFFFFFFFFFFFFFFFL);
+        $this->assertEquals(255, $r->hdr()->uint8());
+        $this->assertEquals(65535, $r->hdr()->uint16());
+        $this->assertEquals(4294967295, $r->hdr()->uint32());
+        // PHP does not have unsigned integers, it has only signed integers in
+        // the range [PHP_INT_MIN, PHP_INT_MAX], so to represent 2^64 - 1 we
+        // need to use an integer with the same internal binary representation, i.e. -1.
+        $this->assertEquals(-1, $r->hdr()->uint64());
 
-        $this->assertEquals(r.hdr().sint8(), -1);
-        $this->assertEquals(r.hdr().sint16(), -1);
-        $this->assertEquals(r.hdr().sint32(), -1);
-        $this->assertEquals(r.hdr().sint64(), -1);
+        $this->assertEquals(-1, $r->hdr()->sint8());
+        $this->assertEquals(-1, $r->hdr()->sint16());
+        $this->assertEquals(-1, $r->hdr()->sint32());
+        $this->assertEquals(-1, $r->hdr()->sint64());
 
-        $this->assertEquals(r.hdr().uint16le(), 66);
-        $this->assertEquals(r.hdr().uint32le(), 66);
-        $this->assertEquals(r.hdr().uint64le(), 66);
+        $this->assertEquals(66, $r->hdr()->uint16le());
+        $this->assertEquals(66, $r->hdr()->uint32le());
+        $this->assertEquals(66, $r->hdr()->uint64le());
 
-        $this->assertEquals(r.hdr().sint16le(), -66);
-        $this->assertEquals(r.hdr().sint32le(), -66);
-        $this->assertEquals(r.hdr().sint64le(), -66);
+        $this->assertEquals(-66, $r->hdr()->sint16le());
+        $this->assertEquals(-66, $r->hdr()->sint32le());
+        $this->assertEquals(-66, $r->hdr()->sint64le());
 
-        $this->assertEquals(r.hdr().uint16be(), 66);
-        $this->assertEquals(r.hdr().uint32be(), 66);
-        $this->assertEquals(r.hdr().uint64be(), 66);
+        $this->assertEquals(66, $r->hdr()->uint16be());
+        $this->assertEquals(66, $r->hdr()->uint32be());
+        $this->assertEquals(66, $r->hdr()->uint64be());
 
-        $this->assertEquals(r.hdr().sint16be(), -66);
-        $this->assertEquals(r.hdr().sint32be(), -66);
-        $this->assertEquals(r.hdr().sint64be(), -66);
-*/    
+        $this->assertEquals(-66, $r->hdr()->sint16be());
+        $this->assertEquals(-66, $r->hdr()->sint32be());
+        $this->assertEquals(-66, $r->hdr()->sint64be());
     }
 }
