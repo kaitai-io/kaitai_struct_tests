f02c83e35d7bbe8a34f3261c5e26b708df76faa4
markbook2 <morpho-os@users.noreply.github.com>
2016-09-03 20:53:32 +0300

    Fixed order of arguments for assertion

--- spec/php/PositionInSeqTest.php
+++ spec/php/PositionInSeqTest.php
@@ -5,6 +5,6 @@ class PositionInSeqTest extends TestCase {
     public function testPositionInSeq() {
         $r = PositionInSeq::fromFile(self::SRC_DIR_PATH . "/position_in_seq.bin");
 
-        $this->assertEquals($r->numbers(), [1, 2, 3]);
+        $this->assertEquals([1, 2, 3], $r->numbers());
     }
 }
