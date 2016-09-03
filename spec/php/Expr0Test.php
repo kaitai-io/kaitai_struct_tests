c680b9e49f014ba9620f16a62013a3d31615e385
markbook2 <morpho-os@users.noreply.github.com>
2016-09-03 20:23:49 +0300

    Replaced expressions: 'property fetch' -> 'method call'

--- spec/php/Expr0Test.php
+++ spec/php/Expr0Test.php
@@ -5,7 +5,7 @@ class Expr0Test extends TestCase {
     public function testExpr0() {
         $r = Expr0::fromFile(self::SRC_DIR_PATH . "/str_encodings.bin");
 
-        $this->assertEquals(0xf7, $r->mustBeF7);
-        $this->assertEquals("abc123", $r->mustBeAbc123);
+        $this->assertEquals(0xf7, $r->mustBeF7());
+        $this->assertEquals("abc123", $r->mustBeAbc123());
     }
 }
