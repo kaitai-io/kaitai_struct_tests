4563c35a2ae256814f956f02312934dbb631189f
markbook2 <morpho-os@users.noreply.github.com>
2016-09-03 20:54:03 +0300

    Fixed PHP syntax errors and order of arguments for the assertions

--- spec/php/ProcessXorValueTest.php
+++ spec/php/ProcessXorValueTest.php
@@ -3,12 +3,9 @@ namespace Kaitai\Struct\Tests;
 
 class ProcessXorValueTest extends TestCase {
     public function testProcessXorValue() {
-        $this->markTestIncomplete();
-/*
-        ProcessXorValue r = ProcessXorValue::fromFile(self::SRC_DIR_PATH . "process_xor_1.bin");
+        $r = ProcessXorValue::fromFile(self::SRC_DIR_PATH . "/process_xor_1.bin");
 
-        $this->assertEquals(r.key(), 0xff);
-        $this->assertEquals(new String(r.buf(), "UTF-8"), "foo bar");
-*/    
+        $this->assertEquals(0xff, $r->key());
+        $this->assertEquals("foo bar", $r->buf());
     }
 }
