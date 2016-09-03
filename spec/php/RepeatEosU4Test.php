c273213a2cbcdeb13e6a4bf5bbdb05bcdd443047
markbook2 <morpho-os@users.noreply.github.com>
2016-09-03 21:01:03 +0300

    Replaced expressions: 'property fetch' -> 'method call'

--- spec/php/RepeatEosU4Test.php
+++ spec/php/RepeatEosU4Test.php
@@ -4,6 +4,6 @@ namespace Kaitai\Struct\Tests;
 class RepeatEosU4Test extends TestCase {
     public function testRepeatEosU4() {
         $r = RepeatEosU4::fromFile(self::SRC_DIR_PATH . "/repeat_eos_struct.bin");
-        $this->assertEquals([0, 0x42, 0x42, 0x815], $r->numbers);
+        $this->assertEquals([0, 0x42, 0x42, 0x815], $r->numbers());
     }
 }
