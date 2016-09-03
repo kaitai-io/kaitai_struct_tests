eb1b3e9ef16fd05491458ed9f03392d83781b966
markbook2 <morpho-os@users.noreply.github.com>
2016-09-03 20:48:46 +0300

    Fixed PHP syntax errors and order of arguments for the assertions

--- spec/php/ProcessXor4ConstTest.php
+++ spec/php/ProcessXor4ConstTest.php
@@ -3,11 +3,7 @@ namespace Kaitai\Struct\Tests;
 
 class ProcessXor4ConstTest extends TestCase {
     public function testProcessXor4Const() {
-        $this->markTestIncomplete();
-/*
-        ProcessXor4Const r = ProcessXor4Const::fromFile(self::SRC_DIR_PATH . "process_xor_4.bin");
-
-        $this->assertEquals(new String(r.buf(), "UTF-8"), "foo bar");
-*/    
+        $r = ProcessXor4Const::fromFile(self::SRC_DIR_PATH . "/process_xor_4.bin");
+        $this->assertEquals("foo bar", $r->buf());
     }
 }
