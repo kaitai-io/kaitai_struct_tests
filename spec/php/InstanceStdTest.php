e3bd161699a398d64374742dab7c77794f9f27b8
markbook2 <morpho-os@users.noreply.github.com>
2016-09-03 20:28:54 +0300

    Fixed PHP syntax errors

--- spec/php/InstanceStdTest.php
+++ spec/php/InstanceStdTest.php
@@ -3,11 +3,8 @@ namespace Kaitai\Struct\Tests;
 
 class InstanceStdTest extends TestCase {
     public function testInstanceStd() {
-        $this->markTestIncomplete();
-/*
-        InstanceStd r = InstanceStd::fromFile(self::SRC_DIR_PATH . "str_encodings.bin");
+        $r = InstanceStd::fromFile(self::SRC_DIR_PATH . "/str_encodings.bin");
 
-        $this->assertEquals(r.header(), "Some ");
-*/    
+        $this->assertEquals("Some ", $r->header());
     }
 }
