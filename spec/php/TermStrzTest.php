e7cd60abef800a5666751d4b1caf1aaa7eb9a297
markbook2 <morpho-os@users.noreply.github.com>
2016-09-03 21:03:44 +0300

    Fixed PHP syntax errors and order of arguments for the assertions

--- spec/php/TermStrzTest.php
+++ spec/php/TermStrzTest.php
@@ -3,13 +3,10 @@ namespace Kaitai\Struct\Tests;
 
 class TermStrzTest extends TestCase {
     public function testTermStrz() {
-        $this->markTestIncomplete();
-/*
-        TermStrz r = TermStrz::fromFile(self::SRC_DIR_PATH . "term_strz.bin");
+        $r = TermStrz::fromFile(self::SRC_DIR_PATH . "/term_strz.bin");
 
-        $this->assertEquals(r.s1(), "foo");
-        $this->assertEquals(r.s2(), "bar");
-        $this->assertEquals(r.s3(), "|baz@");
-*/    
+        $this->assertEquals("foo", $r->s1());
+        $this->assertEquals("bar", $r->s2());
+        $this->assertEquals("|baz@", $r->s3());
     }
 }
