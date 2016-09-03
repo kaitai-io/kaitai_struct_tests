0688046a632e28abd28e6b8ebb218660dc1b3e16
markbook2 <morpho-os@users.noreply.github.com>
2016-09-03 21:00:18 +0300

    Fixed PHP syntax errors and order of arguments for the assertions

--- spec/php/RepeatEosStructTest.php
+++ spec/php/RepeatEosStructTest.php
@@ -3,15 +3,12 @@ namespace Kaitai\Struct\Tests;
 
 class RepeatEosStructTest extends TestCase {
     public function testRepeatEosStruct() {
-        $this->markTestIncomplete();
-/*
-        RepeatEosStruct r = RepeatEosStruct::fromFile(self::SRC_DIR_PATH . "repeat_eos_struct.bin");
+        $r = RepeatEosStruct::fromFile(self::SRC_DIR_PATH . "/repeat_eos_struct.bin");
 
-        $this->assertEquals(r.chunks().size(), 2);
-        $this->assertEquals(r.chunks().get(0).offset(), 0);
-        $this->assertEquals(r.chunks().get(0).len(), 0x42);
-        $this->assertEquals(r.chunks().get(1).offset(), 0x42);
-        $this->assertEquals(r.chunks().get(1).len(), 0x815);
-*/    
+        $this->assertEquals(2, $r->chunks()->size());
+        $this->assertEquals(0, $r->chunks()->get(0)->offset());
+        $this->assertEquals(0x42, $r->chunks()->get(0)->len());
+        $this->assertEquals(0x42, $r->chunks()->get(1)->offset());
+        $this->assertEquals(0x815, $r->chunks()->get(1)->len());
     }
 }
