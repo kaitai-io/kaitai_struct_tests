9e7a39fa66717fb23527b4f542256e6e3cfd15b8
markbook2 <morpho-os@users.noreply.github.com>
2016-09-03 20:27:49 +0300

    Removed testing of the implicit method calls

--- spec/php/HelloWorldTest.php
+++ spec/php/HelloWorldTest.php
@@ -6,7 +6,5 @@ class HelloWorldTest extends TestCase {
         $r = HelloWorld::fromFile(self::SRC_DIR_PATH . '/fixed_struct.bin');
         // Access property through direct call
         $this->assertEquals(0x50, $r->one());
-        // Access property through __get call
-        $this->assertEquals(0x50, $r->one);
     }
 }
