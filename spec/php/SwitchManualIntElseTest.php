1bcf7a2a5b899f02650fa9954de4123bbc5a2b57
Mikhail Yakshin <greycat@altlinux.org>
2016-09-06 00:26:54 +0300

    Ported switch_manual_int_else to JS

---
+++ spec/php/SwitchManualIntElseTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Kaitai\Struct\Tests;
+
+class SwitchManualIntElseTest extends TestCase {
+    public function testSwitchManualIntElse() {
+        $r = SwitchManualIntElse::fromFile(self::SRC_DIR_PATH . "/switch_opcodes2.bin");
+
+        $this->assertEquals(4, $r->opcodes->size);
+
+        $this->assertEquals(83, $r->opcodes[0]->code);
+        $this->assertEquals('foo', $r->opcodes[0]->body->value);
+
+        $this->assertEquals(88, $r->opcodes[1]->code);
+        $this->assertEquals(0x42, $r->opcodes[1]->body->filler);
+
+        $this->assertEquals(89, $r->opcodes[2]->code);
+        $this->assertEquals(0xcafe, $r->opcodes[2]->body->filler);
+
+        $this->assertEquals(73, $r->opcodes[3]->code);
+        $this->assertEquals(7, $r->opcodes[3]->body->value);
+    }
+}
