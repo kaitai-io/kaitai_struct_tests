bc4d072528ffeca92179ecbd6353fddabe96a4f3
Mikhail Yakshin <greycat@altlinux.org>
2016-09-06 11:16:41 +0300

    Ported expr_3 to JS

---
+++ spec/javascript/test_expr_3.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var testHelper = require('testHelper');
+
+testHelper('Expr3', 'src/fixed_struct.bin', function(r) {
+  assert.equal(r.one, 80);
+  assert.equal(r.two, 'ACK');
+
+  assert.equal(r.three, '@ACK');
+  assert.equal(r.four, '_ACK_');
+  assert.equal(r.isStrEq, true);
+  assert.equal(r.isStrNe, false);
+  assert.equal(r.isStrLt, true);
+  assert.equal(r.isStrGt, false);
+  assert.equal(r.isStrLe, true);
+  assert.equal(r.isStrGe, false);
+  assert.equal(r.isStrLt2, true);
+});
