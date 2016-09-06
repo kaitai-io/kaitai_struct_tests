0941dcc01945b6b39f03650e9b206241c451661e
Mikhail Yakshin <greycat@altlinux.org>
2016-09-06 11:14:30 +0300

    Ported expr_3 to C#

---
+++ spec/csharp/kaitai_struct_csharp_tests/tests/SpecExpr3.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+
+namespace Kaitai
+{
+    [TestFixture]
+    public class SpecExpr3 : CommonSpec
+    {
+        [Test]
+        public void TestExpr3()
+        {
+            var r = Expr3.FromFile(SourceFile("fixed_struct.bin"));
+            Assert.AreEqual(r.One, 80);
+            Assert.AreEqual(r.Two, "ACK");
+    
+            Assert.AreEqual(r.Three, "@ACK");
+            Assert.AreEqual(r.Four, "_ACK_");
+            Assert.AreEqual(r.IsStrEq, true);
+            Assert.AreEqual(r.IsStrNe, false);
+            Assert.AreEqual(r.IsStrLt, true);
+            Assert.AreEqual(r.IsStrGt, false);
+            Assert.AreEqual(r.IsStrLe, true);
+            Assert.AreEqual(r.IsStrGe, false);
+            Assert.AreEqual(r.IsStrLt2, true);
+        }
+    }
+}
