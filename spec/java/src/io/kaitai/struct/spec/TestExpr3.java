1f140180476eccf7cb6516b5535904836ddc1af7
Mikhail Yakshin <greycat@altlinux.org>
2016-09-06 11:10:50 +0300

    Ported expr_3 to Java

---
+++ spec/java/src/io/kaitai/struct/spec/TestExpr3.java
@@ -0,0 +1,26 @@
+package io.kaitai.struct.spec;
+
+import io.kaitai.struct.testformats.Expr3;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestExpr3 extends CommonSpec {
+    @Test
+    public void testExpr3() throws Exception {
+        Expr3 r = Expr3.fromFile(SRC_DIR + "fixed_struct.bin");
+
+        assertEquals(r.one(), 80);
+        assertEquals(r.two(), "ACK");
+
+        assertEquals(r.three(), "@ACK");
+        assertEquals(r.four(), "_ACK_");
+        assertEquals(r.isStrEq().booleanValue(), true);
+        assertEquals(r.isStrNe().booleanValue(), false);
+        assertEquals(r.isStrLt().booleanValue(), true);
+        assertEquals(r.isStrGt().booleanValue(), false);
+        assertEquals(r.isStrLe().booleanValue(), true);
+        assertEquals(r.isStrGe().booleanValue(), false);
+        assertEquals(r.isStrLt2().booleanValue(), true);
+    }
+}
