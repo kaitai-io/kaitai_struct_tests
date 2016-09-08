c473443fb3ab27289d077394562f03983ddec3b6
Mikhail Yakshin <greycat@altlinux.org>
2016-09-08 22:31:14 +0300

    Lots of common PHP spec fixes: accessing methods -> accessing properties, array access, array count method

--- spec/php/RepeatEosStructTest.php
+++ spec/php/RepeatEosStructTest.php
@@ -5,10 +5,10 @@ class RepeatEosStructTest extends TestCase {
     public function testRepeatEosStruct() {
         $r = RepeatEosStruct::fromFile(self::SRC_DIR_PATH . "/repeat_eos_struct.bin");
 
-        $this->assertEquals(2, $r->chunks()->size());
-        $this->assertEquals(0, $r->chunks()->get(0)->offset());
-        $this->assertEquals(0x42, $r->chunks()->get(0)->len());
-        $this->assertEquals(0x42, $r->chunks()->get(1)->offset());
-        $this->assertEquals(0x815, $r->chunks()->get(1)->len());
+        $this->assertEquals(2, count($r->chunks));
+        $this->assertEquals(0, $r->chunks[0]->offset);
+        $this->assertEquals(0x42, $r->chunks[0]->len);
+        $this->assertEquals(0x42, $r->chunks[1]->offset);
+        $this->assertEquals(0x815, $r->chunks[1]->len);
     }
 }
--- spec/php/RepeatNStructTest.php
+++ spec/php/RepeatNStructTest.php
@@ -5,10 +5,10 @@ class RepeatNStructTest extends TestCase {
     public function testRepeatNStruct() {
         $r = RepeatNStruct::fromFile(self::SRC_DIR_PATH . "/repeat_n_struct.bin");
 
-        $this->assertEquals(2, $r->qty());
-        $this->assertEquals(0x10, $r->chunks()->get(0)->offset());
-        $this->assertEquals(0x2078,$r->chunks()->get(0)->len());
-        $this->assertEquals(0x2088,$r->chunks()->get(1)->offset());
-        $this->assertEquals(0xf, $r->chunks()->get(1)->len());
+        $this->assertEquals(2, $r->qty);
+        $this->assertEquals(0x10, $r->chunks[0]->offset);
+        $this->assertEquals(0x2078, $r->chunks[0]->len);
+        $this->assertEquals(0x2088, $r->chunks[1]->offset);
+        $this->assertEquals(0xf, $r->chunks[1]->len);
     }
 }
