a650b12ba9fa2deb78de0907968da1cae0521f7d
Mikhail Yakshin <greycat@altlinux.org>
2016-09-22 14:09:49 +0300

    TypeIntUnaryOpTest: fixed PHP port from Java

--- spec/php/TypeIntUnaryOpTest.php
+++ spec/php/TypeIntUnaryOpTest.php
@@ -5,9 +5,9 @@ class TypeIntUnaryOpTest extends TestCase {
     public function testTypeIntUnaryOp() {
         $r = TypeIntUnaryOp::fromFile(self::SRC_DIR_PATH . "/fixed_struct.bin");
 
-        $this->assertEquals(0x4150, $r->valueS2());
-        $this->assertEquals(0x4150ffff312d4b43, $r->valueS8());
-        $this->assertEquals(-0x4150, $r->unaryS2()->shortValue());
-        $this->assertEquals(-0x4150ffff312d4b43, $r->unaryS8()->longValue());
+        $this->assertEquals(0x4150, $r->valueS2);
+        $this->assertEquals(0x4150ffff312d4b43, $r->valueS8);
+        $this->assertEquals(-0x4150, $r->unaryS2);
+        $this->assertEquals(-0x4150ffff312d4b43, $r->unaryS8);
     }
 }
