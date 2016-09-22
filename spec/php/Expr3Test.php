75803d632bd44dff13f900988378ea52789dd4a5
Mikhail Yakshin <greycat@altlinux.org>
2016-09-22 14:10:23 +0300

    Ported expr3 to PHP

---
+++ spec/php/Expr3Test.php
@@ -0,0 +1,21 @@
+<?php
+namespace Kaitai\Struct\Tests;
+
+class Expr3Test extends TestCase {
+    public function testExpr3() {
+        $r = Expr3::fromFile(self::SRC_DIR_PATH . "/fixed_struct.bin");
+
+        $this->assertEquals(80, $r->one);
+        $this->assertEquals('ACK', $r->two);
+
+        $this->assertEquals('@ACK', $r->three);
+        $this->assertEquals('_ACK_', $r->four);
+        $this->assertEquals(true, $r->isStrEq);
+        $this->assertEquals(false, $r->isStrNe);
+        $this->assertEquals(true, $r->isStrLt);
+        $this->assertEquals(false, $r->isStrGt);
+        $this->assertEquals(true, $r->isStrLe);
+        $this->assertEquals(false, $r->isStrGe);
+        $this->assertEquals(true, $r->isStrLt2);
+    }
+}
