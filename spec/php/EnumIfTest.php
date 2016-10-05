072e7ad56f0d998fc8290eb87b0cde3437dc6439
Mikhail Yakshin <greycat@altlinux.org>
2016-10-05 22:43:41 +0300

    PHP: fixed EnumIf test syntax

--- spec/php/EnumIfTest.php
+++ spec/php/EnumIfTest.php
@@ -5,14 +5,14 @@ class EnumIfTest extends TestCase {
     public function testEnumIf() {
         $r = EnumIf::fromFile(self::SRC_DIR_PATH . "/if_struct.bin");
 
-        $this->assertEquals($r->op1()->opcode(), EnumIf::Opcodes()::A_STRING);
+        $this->assertEquals($r->op1()->opcode(), EnumIf\Opcodes::A_STRING);
         $this->assertEquals("foo", $r->op1()->argStr()->str());
 
-        $this->assertEquals($r->op2()->opcode(), EnumIf::Opcodes()::A_TUPLE);
+        $this->assertEquals($r->op2()->opcode(), EnumIf\Opcodes::A_TUPLE);
         $this->assertEquals(0x42, $r->op2()->argTuple()->num1());
         $this->assertEquals(0x43, $r->op2()->argTuple()->num2());
 
-        $this->assertEquals($r->op3()->opcode(), EnumIf::Opcodes()::A_STRING);
+        $this->assertEquals($r->op3()->opcode(), EnumIf\Opcodes::A_STRING);
         $this->assertEquals("bar", $r->op3()->argStr()->str());
     }
 }
