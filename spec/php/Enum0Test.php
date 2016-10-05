1aa8ebe524ded54e2ef465df2bab5df94b638599
Mikhail Yakshin <greycat@altlinux.org>
2016-10-05 19:51:37 +0300

    Fix PHP namespacing syntax

--- spec/php/Enum0Test.php
+++ spec/php/Enum0Test.php
@@ -4,7 +4,7 @@ namespace Kaitai\Struct\Tests;
 class Enum0Test extends TestCase {
     public function testEnum0() {
         $r = Enum0::fromFile(self::SRC_DIR_PATH . "/enum_0.bin");
-        $this->assertEquals($r->pet1(), Enum0::Animal()::CAT);
-        $this->assertEquals($r->pet2(), Enum0::Animal()::CHICKEN);
+        $this->assertEquals($r->pet1(), Enum0\Animal::CAT);
+        $this->assertEquals($r->pet2(), Enum0\Animal::CHICKEN);
     }
 }
