b0979c94727382fd10e535795f0e42ff04262c68
Mikhail Yakshin <greycat@altlinux.org>
2016-10-11 14:42:29 +0300

    Python hello_world: remove extra #close call

--- spec/python/test_hello_world.py
+++ spec/python/test_hello_world.py
@@ -6,4 +6,3 @@ class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         with HelloWorld.from_file('src/fixed_struct.bin') as r:
             self.assertEqual(r.one, 0x50)
-            r._io.close()
