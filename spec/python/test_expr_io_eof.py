e448a05ec45c1f19974621ca24057b97d55a147d
Mikhail Yakshin <greycat@altlinux.org>
2016-10-28 16:48:55 +0300

    Ported expr_io_eof to Python

---
+++ spec/python/test_expr_io_eof.py
@@ -0,0 +1,13 @@
+import unittest
+
+from expr_io_eof import ExprIoEof
+
+class TestExprIoEof(unittest.TestCase):
+    def test_expr_io_eof(self):
+        r = ExprIoEof.from_file("src/fixed_struct.bin")
+
+        self.assertEquals(r.substream1.one, 1262698832)
+        self.assertEquals(hasattr(r.substream1, 'two'), False)
+
+        self.assertEquals(r.substream2.one, 4294914349)
+        self.assertEquals(r.substream2.two, 1262698832)
