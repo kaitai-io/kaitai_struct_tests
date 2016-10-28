75dace2937c7ebbef21724c91c8d49545f33ecd5
Mikhail Yakshin <greycat@altlinux.org>
2016-10-28 17:28:04 +0300

    Ported expr_io_eof to C#

---
+++ spec/csharp/kaitai_struct_csharp_tests/tests/SpecExprIoEof.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Kaitai
+{
+    [TestFixture]
+    public class SpecExprIoEof : CommonSpec
+    {
+        [Test]
+        public void TestExprIoEof()
+        {
+            var r = ExprIoEof.FromFile(SourceFile("fixed_struct.bin"));
+            Assert.AreEqual(r.Substream1.One, 1262698832);
+            Assert.IsNull(r.Substream1.Two);
+    
+            Assert.AreEqual(r.Substream2.One, 4294914349);
+            Assert.AreEqual(r.Substream2.Two, 1262698832);
+        }
+    }
+}
