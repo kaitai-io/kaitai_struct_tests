1f67db0f59787e977e945e425a051285485ccb8e
Mikhail Yakshin <greycat@altlinux.org>
2016-10-31 18:30:47 +0300

    Ported expr_io_pos to Python

---
+++ spec/python/test_expr_io_pos.py
@@ -0,0 +1,15 @@
+import unittest
+
+from expr_io_pos import ExprIoPos
+
+class TestExprIoPos(unittest.TestCase):
+    def test_expr_io_pos(self):
+        r = ExprIoPos.from_file("src/expr_io_pos.bin")
+
+        self.assertEquals(r.substream1.my_str, "CURIOSITY")
+        self.assertEquals(r.substream1.body, bytearray([0x11, 0x22, 0x33, 0x44]))
+        self.assertEquals(r.substream1.number, 0x42)
+
+        self.assertEquals(r.substream2.my_str, "KILLED")
+        self.assertEquals(r.substream2.body, b"a cat")
+        self.assertEquals(r.substream2.number, 0x67)
