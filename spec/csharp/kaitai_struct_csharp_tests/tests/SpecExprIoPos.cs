c99929d29db620c19195397299fd9234b03642b0
Mikhail Yakshin <greycat@altlinux.org>
2016-11-07 13:42:05 +0300

    Fixed expr_io_pos for C# syntax

--- spec/csharp/kaitai_struct_csharp_tests/tests/SpecExprIoPos.cs
+++ spec/csharp/kaitai_struct_csharp_tests/tests/SpecExprIoPos.cs
@@ -9,11 +9,11 @@ namespace Kaitai
         public void TestExprIoPos()
         {
             var r = ExprIoPos.FromFile(SourceFile("expr_io_pos.bin"));
-            Assert.AreEqual(r.Substream1.MyStr, 'CURIOSITY');
+            Assert.AreEqual(r.Substream1.MyStr, "CURIOSITY");
             Assert.AreEqual(r.Substream1.Body, new byte[] { 0x11, 0x22, 0x33, 0x44 });
             Assert.AreEqual(r.Substream1.Number, 0x42);
     
-            Assert.AreEqual(r.Substream2.MyStr, 'KILLED');
+            Assert.AreEqual(r.Substream2.MyStr, "KILLED");
             Assert.AreEqual(r.Substream2.Body, new byte[] { 0x61, 0x20, 0x63, 0x61, 0x74 });
             Assert.AreEqual(r.Substream2.Number, 0x67);
         }
