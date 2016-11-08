44890924173e9cce55455a745282040e93b3636b
Mikhail Yakshin <greycat@altlinux.org>
2016-11-08 20:00:43 +0300

    Rewrote in block style, should fix Python 3 warning

--- spec/python/test_eof_exception_bytes.py
+++ spec/python/test_eof_exception_bytes.py
@@ -4,4 +4,6 @@ from eof_exception_bytes import EofExceptionBytes
 
 class TestEofExceptionBytes(unittest.TestCase):
     def test_eof_exception_bytes(self):
-        self.assertRaises(EOFError, EofExceptionBytes.from_file, "src/term_strz.bin")
+        with self.assertRaises(EOFError):
+            with EofExceptionBytes.from_file("src/term_strz.bin"):
+                pass
