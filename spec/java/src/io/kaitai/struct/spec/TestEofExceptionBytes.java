66aa359dff98dfe26aa2fb218e43f01a2ecb3416
Mikhail Yakshin <greycat@altlinux.org>
2016-11-09 20:22:28 +0300

    Switched to use new exception classes for mmap-based runtime

--- spec/java/src/io/kaitai/struct/spec/TestEofExceptionBytes.java
+++ spec/java/src/io/kaitai/struct/spec/TestEofExceptionBytes.java
@@ -4,13 +4,13 @@ import io.kaitai.struct.testformats.EofExceptionBytes;
 import io.kaitai.struct.testformats.StrEncodings;
 import org.testng.annotations.Test;
 
-import java.io.EOFException;
+import java.nio.BufferUnderflowException;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertThrows;
 
 public class TestEofExceptionBytes extends CommonSpec {
-    @Test(expectedExceptions = EOFException.class)
+    @Test(expectedExceptions = BufferUnderflowException.class)
     public void testEofExceptionBytes() throws Exception {
         EofExceptionBytes r = EofExceptionBytes.fromFile(SRC_DIR + "term_strz.bin");
     }
--- spec/java/src/io/kaitai/struct/spec/TestEofExceptionU4.java
+++ spec/java/src/io/kaitai/struct/spec/TestEofExceptionU4.java
@@ -3,12 +3,12 @@ package io.kaitai.struct.spec;
 import io.kaitai.struct.testformats.EofExceptionU4;
 import org.testng.annotations.Test;
 
-import java.io.EOFException;
+import java.nio.BufferUnderflowException;
 
 import static org.testng.Assert.assertEquals;
 
 public class TestEofExceptionU4 extends CommonSpec {
-    @Test(expectedExceptions = EOFException.class)
+    @Test(expectedExceptions = BufferUnderflowException.class)
     public void testEofExceptionU4() throws Exception {
         EofExceptionU4 r = EofExceptionU4.fromFile(SRC_DIR + "term_strz.bin");
     }
