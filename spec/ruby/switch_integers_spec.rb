e40db0d9d41e5bd510afb3ba43f5a091c41ff204
Mikhail Yakshin <greycat@altlinux.org>
2016-11-21 13:39:25 +0300

    Added switch_integers test + Ruby spec

---
+++ spec/ruby/switch_integers_spec.rb
@@ -0,0 +1,21 @@
+require 'switch_integers'
+
+RSpec.describe SwitchIntegers do
+  it 'parses test properly' do
+    r = SwitchIntegers.from_file('src/switch_integers.bin')
+
+    expect(r.opcodes.size).to eq 4
+
+    expect(r.opcodes[0].code).to eq 1
+    expect(r.opcodes[0].body).to eq 7
+
+    expect(r.opcodes[1].code).to eq 2
+    expect(r.opcodes[1].body).to eq 0x4040
+
+    expect(r.opcodes[2].code).to eq 4
+    expect(r.opcodes[2].body).to eq 4919
+
+    expect(r.opcodes[3].code).to eq 8
+    expect(r.opcodes[3].body).to eq 4919
+  end
+end
