b5eb2346e112ef20c808fa947b4ea1f260d53c01
Mikhail Yakshin <greycat@altlinux.org>
2016-11-22 11:25:37 +0300

    Ported SwitchIntegers to Java

---
+++ spec/java/src/io/kaitai/struct/spec/TestSwitchIntegers.java
@@ -0,0 +1,27 @@
+package io.kaitai.struct.spec;
+
+import io.kaitai.struct.testformats.SwitchIntegers;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestSwitchIntegers extends CommonSpec {
+    @Test
+    public void testSwitchIntegers() throws Exception {
+        SwitchIntegers r = SwitchIntegers.fromFile(SRC_DIR + "switch_integers.bin");
+
+        assertEquals(r.opcodes().size(), 4);
+
+        assertEquals(r.opcodes().get(0).code(), 1);
+        assertEquals(r.opcodes().get(0).body(), 7);
+
+        assertEquals(r.opcodes().get(1).code(), 2);
+        assertEquals(r.opcodes().get(1).body(), 0x4040);
+
+        assertEquals(r.opcodes().get(2).code(), 4);
+        assertEquals(r.opcodes().get(2).body(), 4919);
+
+        assertEquals(r.opcodes().get(3).code(), 8);
+        assertEquals(r.opcodes().get(3).body(), 4919);
+    }
+}
