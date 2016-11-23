0e2f42182412d2fdfd83adf2ddc39dfb765d9f5d
Mikhail Yakshin <greycat@altlinux.org>
2016-11-23 16:12:38 +0300

    Ported ExprEnum to Java

---
+++ spec/java/src/io/kaitai/struct/spec/TestExprEnum.java
@@ -0,0 +1,18 @@
+package io.kaitai.struct.spec;
+
+import io.kaitai.struct.testformats.ExprEnum;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestExprEnum extends CommonSpec {
+    @Test
+    public void testExprEnum() throws Exception {
+        ExprEnum r = ExprEnum.fromFile(SRC_DIR + "term_strz.bin");
+
+        assertEquals(r.constDog(), ExprEnum.Animal.DOG);
+        assertEquals(r.constNobody(), null);
+        assertEquals(r.derivedBoom(), ExprEnum.Animal.BOOM);
+        assertEquals(r.derivedDog(), ExprEnum.Animal.DOG);
+    }
+}
