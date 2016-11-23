1cfbed72b4c9f53a9e765667aec2006cad8b70d3
Mikhail Yakshin <greycat@altlinux.org>
2016-11-23 16:31:48 +0300

    Fixed C# spec for ExprEnum; wow, actually Assert.AreEqual has (expected, actual) arg order

--- spec/csharp/kaitai_struct_csharp_tests/tests/SpecExprEnum.cs
+++ spec/csharp/kaitai_struct_csharp_tests/tests/SpecExprEnum.cs
@@ -9,10 +9,10 @@ namespace Kaitai
         public void TestExprEnum()
         {
             var r = ExprEnum.FromFile(SourceFile("term_strz.bin"));
-            Assert.AreEqual(r.ConstDog, ExprEnum.Animal.Dog);
-            Assert.AreEqual(r.ConstNobody, null);
-            Assert.AreEqual(r.DerivedBoom, ExprEnum.Animal.Boom);
-            Assert.AreEqual(r.DerivedDog, ExprEnum.Animal.Dog);
+            Assert.AreEqual(ExprEnum.Animal.Dog, r.ConstDog);
+            Assert.AreEqual((ExprEnum.Animal) 8, r.ConstNobody);
+            Assert.AreEqual(ExprEnum.Animal.Boom, r.DerivedBoom);
+            Assert.AreEqual(ExprEnum.Animal.Dog, r.DerivedDog);
         }
     }
 }
