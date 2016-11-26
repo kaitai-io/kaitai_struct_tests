2e30a41e808fba5a1077041623b3b43ca4bba12f
Mikhail Yakshin <greycat@altlinux.org>
2016-11-26 22:25:23 +0300

    Added expr_enum spec for Python

---
+++ spec/python/test_expr_enum.py
@@ -0,0 +1,12 @@
+import unittest
+
+from expr_enum import ExprEnum
+
+class TestExprEnum(unittest.TestCase):
+    def test_expr_enum(self):
+        r = ExprEnum.from_file("src/term_strz.bin")
+
+        self.assertEqual(r.const_dog, ExprEnum.Animal.dog)
+        self.assertEqual(r.const_nobody, None)
+        self.assertEqual(r.derived_boom, ExprEnum.Animal.boom)
+        self.assertEqual(r.derived_dog, ExprEnum.Animal.dog)
