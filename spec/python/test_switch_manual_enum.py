ab1def042c8cbbcd685e58269c089a2fa404353d
Mikhail Yakshin <greycat@altlinux.org>
2016-11-26 22:22:40 +0300

    Fixed switch_manual_enum spec for Python - it works, at last

--- spec/python/test_switch_manual_enum.py
+++ spec/python/test_switch_manual_enum.py
@@ -6,16 +6,16 @@ class TestSwitchManualEnum(unittest.TestCase):
     def test_switch_manual_enum(self):
         with SwitchManualEnum.from_file("src/switch_opcodes.bin") as r:
 
-            self.assertEqual(r.opcodes.size, 4)
+            self.assertEqual(len(r.opcodes), 4)
 
-            self.assertEqual(r.opcodes[0].code, SwitchManualEnum.Opcode.Code.strval)
+            self.assertEqual(r.opcodes[0].code, SwitchManualEnum.Opcode.CodeEnum.strval)
             self.assertEqual(r.opcodes[0].body.value, "foobar")
 
-            self.assertEqual(r.opcodes[1].code, SwitchManualEnum.Opcode.Code.intval)
+            self.assertEqual(r.opcodes[1].code, SwitchManualEnum.Opcode.CodeEnum.intval)
             self.assertEqual(r.opcodes[1].body.value, 0x42)
 
-            self.assertEqual(r.opcodes[2].code, SwitchManualEnum.Opcode.Code.intval)
+            self.assertEqual(r.opcodes[2].code, SwitchManualEnum.Opcode.CodeEnum.intval)
             self.assertEqual(r.opcodes[2].body.value, 0x37)
 
-            self.assertEqual(r.opcodes[3].code, SwitchManualEnum.Opcode.Code.strval)
+            self.assertEqual(r.opcodes[3].code, SwitchManualEnum.Opcode.CodeEnum.strval)
             self.assertEqual(r.opcodes[3].body.value, "")
