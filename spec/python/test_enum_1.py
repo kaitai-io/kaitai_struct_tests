14b5d5248b111cde38419245f06cc4875c55d4a3
Mikhail Yakshin <greycat@altlinux.org>
2016-11-26 19:50:49 +0300

    Fixed enum_1 spec for Python

--- spec/python/test_enum_1.py
+++ spec/python/test_enum_1.py
@@ -6,5 +6,5 @@ class TestEnum1(unittest.TestCase):
     def test_enum_1(self):
         r = Enum1.from_file("src/enum_0.bin")
 
-        self.assertEqual(r.main.pet_1, Enum1.MainObj.Animal.cat)
-        self.assertEqual(r.main.pet_2, Enum1.MainObj.Animal.chicken)
+        self.assertEqual(r.main.submain.pet_1, Enum1.MainObj.Animal.cat)
+        self.assertEqual(r.main.submain.pet_2, Enum1.MainObj.Animal.chicken)
