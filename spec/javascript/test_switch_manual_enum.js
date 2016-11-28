106b9a6bd1f0b3be4f76ef82acd6e0fb3516229c
Mikhail Yakshin <greycat@altlinux.org>
2016-11-28 13:36:24 +0300

    Fixed switch_manual_enum spec for JS

--- spec/javascript/test_switch_manual_enum.js
+++ spec/javascript/test_switch_manual_enum.js
@@ -1,18 +1,18 @@
 var assert = require('assert');
 var testHelper = require('testHelper');
 
-testHelper('SwitchManualEnum', 'src/switch_opcodes.bin', function(r) {
+testHelper('SwitchManualEnum', 'src/switch_opcodes.bin', function(r, SwitchManualEnum) {
   assert.equal(r.opcodes.length, 4);
 
-  assert.equal(r.opcodes[0].code, r.Opcode.CodeEnum.STRVAL);
+  assert.equal(r.opcodes[0].code, SwitchManualEnum.Opcode.CodeEnum.STRVAL);
   assert.equal(r.opcodes[0].body.value, 'foobar');
 
-  assert.equal(r.opcodes[1].code, r.Opcode.CodeEnum.INTVAL);
+  assert.equal(r.opcodes[1].code, SwitchManualEnum.Opcode.CodeEnum.INTVAL);
   assert.equal(r.opcodes[1].body.value, 0x42);
 
-  assert.equal(r.opcodes[2].code, r.Opcode.CodeEnum.INTVAL);
+  assert.equal(r.opcodes[2].code, SwitchManualEnum.Opcode.CodeEnum.INTVAL);
   assert.equal(r.opcodes[2].body.value, 0x37);
 
-  assert.equal(r.opcodes[3].code, r.Opcode.CodeEnum.STRVAL);
+  assert.equal(r.opcodes[3].code, SwitchManualEnum.Opcode.CodeEnum.STRVAL);
   assert.equal(r.opcodes[3].body.value, '');
 });
