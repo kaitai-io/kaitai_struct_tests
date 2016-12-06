24cace16e412daa6f5eefa6829a954b1773d1fa4
Mikhail Yakshin <greycat@altlinux.org>
2016-12-06 19:49:44 +0300

    Ported nav_parent2/3 to Python

---
+++ spec/python/test_nav_parent3.py
@@ -0,0 +1,20 @@
+import unittest
+
+from nav_parent3 import NavParent3
+
+class TestNavParent3(unittest.TestCase):
+    def test_nav_parent3(self):
+        r = NavParent3.from_file("src/nav_parent2.bin")
+
+        self.assertEqual(r.ofs_tags, 8)
+        self.assertEqual(r.num_tags, 2)
+
+        self.assertEqual(r.tags[0].name, "RAHC")
+        self.assertEqual(r.tags[0].ofs, 0x20)
+        self.assertEqual(r.tags[0].num_items, 3)
+        self.assertEqual(r.tags[0].tag_content.content, "foo")
+
+        self.assertEqual(r.tags[1].name, "RAHC")
+        self.assertEqual(r.tags[1].ofs, 0x23)
+        self.assertEqual(r.tags[1].num_items, 6)
+        self.assertEqual(r.tags[1].tag_content.content, "barbaz")
