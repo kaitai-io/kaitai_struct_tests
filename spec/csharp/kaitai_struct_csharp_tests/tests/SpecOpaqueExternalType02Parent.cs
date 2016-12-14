55e7c42e77d9219367db56ad63f12b98aa545ee8
Mikhail Yakshin <greycat@altlinux.org>
2016-12-14 06:56:00 +0300

    Ported OpaqueExternalType02Parent to C#

---
+++ spec/csharp/kaitai_struct_csharp_tests/tests/SpecOpaqueExternalType02Parent.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Kaitai
+{
+    [TestFixture]
+    public class SpecOpaqueExternalType02Parent : CommonSpec
+    {
+        [Test]
+        public void TestOpaqueExternalType02Parent()
+        {
+            var r = OpaqueExternalType02Parent.FromFile(SourceFile("term_strz.bin"));
+            Assert.AreEqual(r.Parent.Child.S1, "foo");
+            Assert.AreEqual(r.Parent.Child.S2, "bar");
+            Assert.AreEqual(r.Parent.Child.S3.S3, "|baz@");
+        }
+    }
+}
