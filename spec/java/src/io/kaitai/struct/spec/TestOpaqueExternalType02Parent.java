eaa4075d421246d69b1f5e364c6c22540043a7c5
Mikhail Yakshin <greycat@altlinux.org>
2016-12-14 07:00:33 +0300

    Ported OpaqueExternalType02Parent to Java

---
+++ spec/java/src/io/kaitai/struct/spec/TestOpaqueExternalType02Parent.java
@@ -0,0 +1,17 @@
+package io.kaitai.struct.spec;
+
+import io.kaitai.struct.testformats.OpaqueExternalType02Parent;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestOpaqueExternalType02Parent extends CommonSpec {
+    @Test
+    public void testOpaqueExternalType02Parent() throws Exception {
+        OpaqueExternalType02Parent r = OpaqueExternalType02Parent.fromFile(SRC_DIR + "term_strz.bin");
+
+        assertEquals(r.parent().child().s1(), "foo");
+        assertEquals(r.parent().child().s2(), "bar");
+        assertEquals(r.parent().child().s3().s3(), "|baz@");
+    }
+}
