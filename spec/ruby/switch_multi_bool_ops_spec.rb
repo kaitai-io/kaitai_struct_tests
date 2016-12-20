7070de1077543918271313da01e7a8d62a254073
Thorsten Schöning <tschoening@users.noreply.github.com>
2016-12-20 15:05:12 +0100

    Not sure if it's really necessary, but all tests actually check some values, so I'm copying the value checks from switch_integer here as well, if we already parse the corresponding data file. Parsing the file should be done, because depending on the target language, the tested error might not necessarily occure without it.

--- spec/ruby/switch_multi_bool_ops_spec.rb
+++ spec/ruby/switch_multi_bool_ops_spec.rb
@@ -4,6 +4,18 @@ RSpec.describe SwitchMultiBoolOps do
   it 'parses test properly' do
     r = SwitchMultiBoolOps.from_file('src/switch_integers.bin')
 
-    # The test just needs to compile/execute, we don't care about actual data.
+    expect(r.opcodes.size).to eq 4
+
+    expect(r.opcodes[0].code).to eq 1
+    expect(r.opcodes[0].body).to eq 7
+
+    expect(r.opcodes[1].code).to eq 2
+    expect(r.opcodes[1].body).to eq 0x4040
+
+    expect(r.opcodes[2].code).to eq 4
+    expect(r.opcodes[2].body).to eq 4919
+
+    expect(r.opcodes[3].code).to eq 8
+    expect(r.opcodes[3].body).to eq 4919
   end
 end
