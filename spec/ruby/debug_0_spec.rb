7b0c766eb1bb7ad41d941455aae390e011868941
Mikhail Yakshin <greycat@altlinux.org>
2017-01-01 11:32:54 +0300

    Ruby debug_0 spec: added some real tests for debug functionality

--- spec/ruby/debug_0_spec.rb
+++ spec/ruby/debug_0_spec.rb
@@ -7,5 +7,20 @@ RSpec.describe Debug0 do
 
     expect(r.one).to eq 0x50
     expect(r.array_of_ints).to eq [0x41, 0x43, 0x4b]
+
+    expect(Debug0::SEQ_FIELDS).to eq ['one', 'array_of_ints']
+    expect(r._debug['one']).to eq({
+      start: 0,
+      end: 1,
+    })
+    expect(r._debug['array_of_ints']).to eq({
+      start: 1,
+      end: 4,
+      arr: [
+        {start: 1, end: 2},
+        {start: 2, end: 3},
+        {start: 3, end: 4},
+      ]
+    })
   end
 end
