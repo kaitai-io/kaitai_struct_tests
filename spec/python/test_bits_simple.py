cdb787aa1d04ce6f94d218535782b8d84b4dab07
Mikhail Yakshin <greycat@altlinux.org>
2017-01-04 12:15:34 +0300

    Ported bits_simple to Python

---
+++ spec/python/test_bits_simple.py
@@ -0,0 +1,27 @@
+import unittest
+
+from bits_simple import BitsSimple
+
+class TestBitsSimple(unittest.TestCase):
+    def test_bits_simple(self):
+        r = BitsSimple.from_file("src/fixed_struct.bin")
+
+        # 50 41
+        self.assertEqual(r.byte_1, 0x50)
+        self.assertEqual(r.byte_2, 0x41)
+
+        # 43 (1 + 3 + 4) = 0|100|0011
+        self.assertEqual(r.bits_a, 0)
+        self.assertEqual(r.bits_b, 0b100)
+        self.assertEqual(r.bits_c, 0b0011)
+
+        # 4B 2D 31 (10 + 3 + 11) = 01001011 00|101|101 00110001
+        self.assertEqual(r.large_bits_1, 0b0100101100)
+        self.assertEqual(r.spacer, 0b101)
+        self.assertEqual(r.large_bits_2, 0b10100110001)
+
+        # FF FF
+        self.assertEqual(r.normal_s2, -1)
+
+        # 50 41 43
+        self.assertEqual(r.byte_8_9_10, 0x504143)
