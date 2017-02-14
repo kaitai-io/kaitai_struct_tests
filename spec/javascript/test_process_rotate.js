92a83423d48053661cc2eea97508d9b44f16f130
Mikhail Yakshin <greycat@altlinux.org>
2017-02-14 19:23:43 +0300

    Fix arrayToString -> bytesToStr to maintain same runtime API names in all languages

--- spec/javascript/test_process_rotate.js
+++ spec/javascript/test_process_rotate.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var testHelper = require('testHelper');
 
 testHelper('ProcessRotate', 'src/process_rotate.bin', function(r) {
-    assert.equal(KaitaiStream.arrayToString(r.buf1, "UTF-8"), "Hello");
-    assert.equal(KaitaiStream.arrayToString(r.buf2, "UTF-8"), "World");
-    assert.equal(KaitaiStream.arrayToString(r.buf3, "UTF-8"), "There");
+    assert.equal(KaitaiStream.bytesToStr(r.buf1, "UTF-8"), "Hello");
+    assert.equal(KaitaiStream.bytesToStr(r.buf2, "UTF-8"), "World");
+    assert.equal(KaitaiStream.bytesToStr(r.buf3, "UTF-8"), "There");
 });
