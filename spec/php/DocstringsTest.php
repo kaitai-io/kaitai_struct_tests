e38d2f348db98e7954692dd68d93aa8feb87bb06
markbook2 <markbook2@users.noreply.github.com>
2017-03-01 07:27:04 +0300

    Added markTestAsNotRisky() and marked some tests as skipped and some as not risky

--- spec/php/DocstringsTest.php
+++ spec/php/DocstringsTest.php
@@ -4,5 +4,6 @@ namespace Kaitai\Struct\Tests;
 class DocstringsTest extends TestCase {
     public function testDocstrings() {
         $r = Docstrings::fromFile(self::SRC_DIR_PATH . "/fixed_struct.bin");
+        $this->markTestAsNotRisky();
     }
 }
