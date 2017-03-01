09ac725f62bcc12a2927cb117bd950a99a64b2ef
Mikhail Yakshin <greycat@altlinux.org>
2017-03-01 09:06:03 +0300

    Renamed Ruby's parent_vs_value_inst -> nav_parent_vs_value_inst

--- spec/ruby/parent_vs_value_inst_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'parent_vs_value_inst'
-
-RSpec.describe ParentVsValueInst do
-  it 'parses test properly' do
-    r = ParentVsValueInst.from_file('src/term_strz.bin')
-
-    expect(r.s1).to eq('foo')
-  end
-end
