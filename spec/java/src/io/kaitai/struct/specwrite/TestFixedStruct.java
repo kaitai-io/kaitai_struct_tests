8e1d1d057128ef91ab0540f36d320d6ab1576784
Mikhail Yakshin <greycat@altlinux.org>
2017-03-24 11:40:21 +0300

    Ported fixed_struct to writing tests in Java

---
+++ spec/java/src/io/kaitai/struct/specwrite/TestFixedStruct.java
@@ -0,0 +1,42 @@
+package io.kaitai.struct.specwrite;
+
+import io.kaitai.struct.testwrite.FixedStruct;
+import org.testng.annotations.Test;
+
+public class TestFixedStruct extends CommonSpec {
+    @Test
+    public void testFixedStruct() throws Exception {
+        FixedStruct.Header r = new FixedStruct.Header(emptyIO());
+
+        r.setUint8(255);
+        r.setUint16(65535);
+        r.setUint32(4294967295L);
+        //r.setUint64(18446744073709551615);
+        r.setUint64(0xFFFFFFFFFFFFFFFFL);
+
+        r.setSint8((byte) -1);
+        r.setSint16((short) -1);
+        r.setSint32(-1);
+        r.setSint64(-1);
+
+        r.setUint16le(66);
+        r.setUint32le(66);
+        r.setUint64le(66);
+
+        r.setSint16le((short) -66);
+        r.setSint32le(-66);
+        r.setSint64le(-66);
+
+        r.setUint16be(66);
+        r.setUint32be(66);
+        r.setUint64be(66);
+
+        r.setSint16be((short) -66);
+        r.setSint32be(-66);
+        r.setSint64be(-66);
+        
+        r._write();
+
+        assertEqualToFile(r, "fixed_struct.bin");
+    }
+}
