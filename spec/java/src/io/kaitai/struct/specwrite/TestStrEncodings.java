803209fb9929bcdc2ffbd9416050b5170d9349f3
Mikhail Yakshin <greycat@altlinux.org>
2017-03-30 11:04:21 +0300

    specwrite/TestStrEncodings: added string size _check testing

--- spec/java/src/io/kaitai/struct/specwrite/TestStrEncodings.java
+++ spec/java/src/io/kaitai/struct/specwrite/TestStrEncodings.java
@@ -1,5 +1,6 @@
 package io.kaitai.struct.specwrite;
 
+import io.kaitai.struct.ConsistencyError;
 import io.kaitai.struct.testwrite.StrEncodings;
 import org.testng.annotations.Test;
 
@@ -20,5 +21,33 @@ public class TestStrEncodings extends CommonSpec {
         r.setLenOf4(3);
 
         assertEqualToFile(r, "str_encodings.bin");
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void checkNull() throws Exception {
+        StrEncodings r = new StrEncodings();
+
+        r.setStr1("woo");
+        r.setLenOf1(3);
+
+        r._check();
+    }
+
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: str2,.*")
+    public void checkMismatch() throws Exception {
+        StrEncodings r = new StrEncodings();
+
+        r.setStr1("Some ASCII");
+        r.setStr2("こんにちは");
+        r.setStr3("こんにちは");
+        r.setStr4("░▒▓");
+
+        // To be auto-derived
+        r.setLenOf1(10);
+        r.setLenOf2(12); // should be 15
+        r.setLenOf3(10);
+        r.setLenOf4(3);
+
+        r._check();
     }
 }
