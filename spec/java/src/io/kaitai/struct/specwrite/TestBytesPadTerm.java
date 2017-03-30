2cc29ff5d58eb4db62b2ba574ddbf28d8d8e858f
Mikhail Yakshin <greycat@altlinux.org>
2017-03-30 23:50:38 +0300

    Added more byte limited type consistency check tests

--- spec/java/src/io/kaitai/struct/specwrite/TestBytesPadTerm.java
+++ spec/java/src/io/kaitai/struct/specwrite/TestBytesPadTerm.java
@@ -1,5 +1,6 @@
 package io.kaitai.struct.specwrite;
 
+import io.kaitai.struct.ConsistencyError;
 import io.kaitai.struct.testwrite.BytesPadTerm;
 import org.testng.annotations.Test;
 
@@ -14,5 +15,49 @@ public class TestBytesPadTerm extends CommonSpec {
         r.setStrTermInclude("str4baz@".getBytes());
 
         assertEqualToFile(r, "str_pad_term.bin");
+    }
+
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: str_pad,.*")
+    public void checkLongerString1() throws Exception {
+        BytesPadTerm r = new BytesPadTerm();
+        r.setStrPad("1234567890123456789012345".getBytes());
+        r._check();
+    }
+
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: str_term,.*")
+    public void checkLongerString2() throws Exception {
+        BytesPadTerm r = new BytesPadTerm();
+        r.setStrPad("12345678901234567890".getBytes());
+        r.setStrTerm("12345678901234567890".getBytes());
+        r._check();
+    }
+
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: str_term_and_pad,.*")
+    public void checkLongerString3() throws Exception {
+        BytesPadTerm r = new BytesPadTerm();
+        r.setStrPad("12345678901234567890".getBytes());
+        r.setStrTerm("1234567890123456789".getBytes());
+        r.setStrTermAndPad("123456789012345678901".getBytes());
+        r._check();
+    }
+
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: str_term_include,.*")
+    public void checkLongerString4() throws Exception {
+        BytesPadTerm r = new BytesPadTerm();
+        r.setStrPad("12345678901234567890".getBytes());
+        r.setStrTerm("1234567890123456789".getBytes());
+        r.setStrTermAndPad("12345678901234567890".getBytes());
+        r.setStrTermInclude("123456789012345678901".getBytes());
+        r._check();
+    }
+
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: str_term_include,.*")
+    public void checkBadTerminator() throws Exception {
+        BytesPadTerm r = new BytesPadTerm();
+        r.setStrPad("12345678901234567890".getBytes());
+        r.setStrTerm("1234567890123456789".getBytes());
+        r.setStrTermAndPad("12345678901234567890".getBytes());
+        r.setStrTermInclude("123".getBytes());
+        r._check();
     }
 }
