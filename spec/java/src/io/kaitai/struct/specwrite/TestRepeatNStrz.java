a00d702b71fe8d42f67342e99d739dba59edba16
Mikhail Yakshin <greycat@altlinux.org>
2017-03-30 05:42:35 +0300

    Added tests for _check number of members in array mismatch + NPE

--- spec/java/src/io/kaitai/struct/specwrite/TestRepeatNStrz.java
+++ spec/java/src/io/kaitai/struct/specwrite/TestRepeatNStrz.java
@@ -1,5 +1,6 @@
 package io.kaitai.struct.specwrite;
 
+import io.kaitai.struct.ConsistencyError;
 import io.kaitai.struct.testwrite.RepeatNStrz;
 import org.testng.annotations.Test;
 
@@ -8,12 +9,29 @@ import java.util.Arrays;
 
 public class TestRepeatNStrz extends CommonSpec {
     @Test
-    public void testRepeatNStrz() throws Exception {
+    public void test() throws Exception {
         RepeatNStrz r = new RepeatNStrz();
 
         r.setQty(2);
         r.setLines(new ArrayList<>(Arrays.asList("foo", "bar")));
 
         assertEqualToFile(r, "repeat_n_strz.bin");
+    }
+
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: lines,.*")
+    public void checkMismatch() throws Exception {
+        RepeatNStrz r = new RepeatNStrz();
+
+        r.setQty(7);
+        r.setLines(new ArrayList<>(Arrays.asList("foo", "bar")));
+
+        r._check();
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void checkNull() throws Exception {
+        RepeatNStrz r = new RepeatNStrz();
+
+        r._check();
     }
 }
