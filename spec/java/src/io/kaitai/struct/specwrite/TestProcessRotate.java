99b1753b3d0f009b89a80106e2cecc31c3dc4594
Mikhail Yakshin <greycat@altlinux.org>
2017-03-30 11:19:23 +0300

    Added byte array limited length check test

--- spec/java/src/io/kaitai/struct/specwrite/TestProcessRotate.java
+++ spec/java/src/io/kaitai/struct/specwrite/TestProcessRotate.java
@@ -1,5 +1,6 @@
 package io.kaitai.struct.specwrite;
 
+import io.kaitai.struct.ConsistencyError;
 import io.kaitai.struct.testwrite.ProcessRotate;
 import org.testng.annotations.Test;
 
@@ -14,5 +15,15 @@ public class TestProcessRotate extends CommonSpec {
         r.setBuf3("There".getBytes());
 
         assertEqualToFile(r, "process_rotate.bin");
+    }
+
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: buf2,.*")
+    public void checkSizeMismatch() throws Exception {
+        ProcessRotate r = new ProcessRotate();
+
+        r.setBuf1("Hello".getBytes());
+        r.setBuf2("Way too long".getBytes());
+
+        r._check();
     }
 }
