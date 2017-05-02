03266ed423a3bc848af3e0e703d6791b4081b9d2
Mikhail Yakshin <greycat@altlinux.org>
2017-05-02 15:25:18 +0300

    aggregate: added parsing of Go failed format builds

--- aggregate/build_fail_parser.rb
+++ aggregate/build_fail_parser.rb
@@ -7,13 +7,23 @@ class BuildFailParser
     fn = "#{@dir}/build.fails"
     File.open(fn, 'r') { |f|
       f.each_line { |l|
-        case l
+        case l.chomp
         when /^Test(.*?)\.java$/
           name = $1
           yield TestResult.new(name, :spec_build_failed, 0, nil)
         when /^(.*?)\.java$/
           name = $1
           yield TestResult.new(name, :format_build_failed, 0, nil)
+        when /^(.*?)\.go:(\d+): (.*?)$/
+          fn = $1
+          name = underscore_to_ucamelcase(File.basename(fn))
+          fail = TestResult::Failure.new(
+            fn,
+            $2,
+            $3,
+            nil
+          )
+          yield TestResult.new(name, :format_build_failed, 0, fail)
         end
       }
     } if FileTest::exists?(fn)
