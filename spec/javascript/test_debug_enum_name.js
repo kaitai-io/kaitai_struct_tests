465906374b8e7ae31780bd17e933737dc7dccdb6
Mikhail Yakshin <greycat@altlinux.org>
2017-05-02 10:36:37 +0300

    debug_enum_name: work around C# naming problems

--- spec/javascript/test_debug_enum_name.js
+++ spec/javascript/test_debug_enum_name.js
@@ -5,8 +5,8 @@ testHelper('DebugEnumName', 'src/fixed_struct.bin', function(r) {
     r._read();
     assert.equal(r._debug.one.enumName, "DebugEnumName.TestEnum1");
     assert.equal(r._debug.arrayOfInts.arr[0].enumName, "DebugEnumName.TestEnum2");
-    assert.equal(r.testType._debug.field1.enumName, "DebugEnumName.TestType.InnerEnum1");
+    assert.equal(r.testType._debug.field1.enumName, "DebugEnumName.TestSubtype.InnerEnum1");
     assert.equal(r.testType._debug._m_instanceField, undefined);
     r.testType.instanceField;
-    assert.equal(r.testType._debug._m_instanceField.enumName, "DebugEnumName.TestType.InnerEnum2");
+    assert.equal(r.testType._debug._m_instanceField.enumName, "DebugEnumName.TestSubtype.InnerEnum2");
 });
