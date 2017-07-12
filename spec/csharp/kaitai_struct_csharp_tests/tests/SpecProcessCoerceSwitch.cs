40217b40c51a15ef98a29430842d9740107ac45d
Mikhail Yakshin <greycat@altlinux.org>
2017-07-12 12:33:09 +0300

    Fixed process_coerce_switch for C#

--- spec/csharp/kaitai_struct_csharp_tests/tests/SpecProcessCoerceSwitch.cs
+++ spec/csharp/kaitai_struct_csharp_tests/tests/SpecProcessCoerceSwitch.cs
@@ -11,7 +11,7 @@ namespace Kaitai
             var r = ProcessCoerceSwitch.FromFile(SourceFile("process_coerce_switch.bin"));
             Assert.AreEqual(r.BufType, 0);
             Assert.AreEqual(r.Flag, 0);
-            Assert.AreEqual(r.Buf.Foo, "AAAA");
+            Assert.AreEqual(((ProcessCoerceSwitch.Foo)r.Buf).Bar, new byte[] { 0x41, 0x41, 0x41, 0x41 });
         }
     }
 }
