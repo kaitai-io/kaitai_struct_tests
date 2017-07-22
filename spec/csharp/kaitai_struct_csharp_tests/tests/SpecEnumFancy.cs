2991651a79403bdc8b4eb9e4ce5ef1e5b6ec4139
Mikhail Yakshin <greycat@altlinux.org>
2017-07-22 16:20:59 +0300

    Fix EnumFancy for C#

--- spec/csharp/kaitai_struct_csharp_tests/tests/SpecEnumFancy.cs
+++ spec/csharp/kaitai_struct_csharp_tests/tests/SpecEnumFancy.cs
@@ -10,7 +10,7 @@ namespace Kaitai
         {
             var r = EnumFancy.FromFile(SourceFile("enum_0.bin"));
             Assert.AreEqual(r.Pet1, EnumFancy.Animal.Cat);
-            Assert.AreEqual(r.Pet2, EnumFancy.Animal.Dog);
+            Assert.AreEqual(r.Pet2, EnumFancy.Animal.Chicken);
         }
     }
 }
