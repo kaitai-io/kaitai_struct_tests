f7c2d395e63a33dc41fd7541776968e8f93102ea
Mikhail Yakshin <greycat@altlinux.org>
2017-07-26 19:26:52 +0300

    Added index_to_param_* + Ruby specs

---
+++ spec/ruby/index_to_param_eos_spec.rb
@@ -0,0 +1,17 @@
+require 'index_to_param_eos'
+
+RSpec.describe IndexToParamEos do
+  it 'parses test properly' do
+    r = IndexToParamEos.from_file('src/index_sizes.bin')
+
+    expect(r.qty).to eq 3
+
+    expect(r.sizes[0]).to eq 1
+    expect(r.sizes[1]).to eq 8
+    expect(r.sizes[2]).to eq 4
+
+    expect(r.blocks[0].buf).to eq "A"
+    expect(r.blocks[1].buf).to eq "BBBBBBBB"
+    expect(r.blocks[2].buf).to eq "CCCC"
+  end
+end
