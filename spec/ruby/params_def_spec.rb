67fa5052b5d0126c5f06911a1b36bfc5f8639c5a
Mikhail Yakshin <greycat@altlinux.org>
2017-07-26 12:22:39 +0300

    Started params_def.ksy + Ruby spec

---
+++ spec/ruby/params_def_spec.rb
@@ -0,0 +1,11 @@
+require 'params_def'
+
+RSpec.describe ParamsDef do
+  it 'parses test properly' do
+    io = Kaitai::Struct::Stream.open('src/term_strz.bin')
+    r = ParamsDef.new(io, nil, nil, 5, true)
+
+    expect(r.buf).to eq('foo|b')
+    expect(r.trailer).to eq(0x61)
+  end
+end
