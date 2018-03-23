6d9f706f14ca287ec5d1d59348e0ea2cefb00c88
Mikhail Yakshin <greycat@altlinux.org>
2018-03-23 14:49:57 +0000

    Started Construct test runner, based on run-python => seems to work!

---
+++ spec/construct/test_hello_world.py
@@ -0,0 +1,8 @@
+import unittest
+
+from hello_world import _schema
+
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world(self):
+        r = _schema.parse_file('src/fixed_struct.bin')
+        self.assertEqual(r.one, 80)
