eca3cc299d75c6dfcaef7a59ab3c845724eecbff
Mikhail Yakshin <greycat@altlinux.org>
2018-09-02 20:12:12 +0100

    Fix spelling

--- spec/csharp/kaitai_struct_csharp_tests/tests/SpecDebugArrayUser.cs
+++ spec/csharp/kaitai_struct_csharp_tests/tests/SpecDebugArrayUser.cs
@@ -13,9 +13,9 @@ namespace Kaitai
             DebugArrayUser r = DebugArrayUser.FromFile(SourceFile("fixed_struct.bin"));
             r._read();
             Assert.AreEqual(r.OneCat.Meow, 0x50);
-            Assert.AreEqual(r.ArraysOfCats[0].Meow, 0x41);
-            Assert.AreEqual(r.ArraysOfCats[1].Meow, 0x43);
-            Assert.AreEqual(r.ArraysOfCats[2].Meow, 0x4b);
+            Assert.AreEqual(r.ArrayOfCats[0].Meow, 0x41);
+            Assert.AreEqual(r.ArrayOfCats[1].Meow, 0x43);
+            Assert.AreEqual(r.ArrayOfCats[2].Meow, 0x4b);
         }
     }
 }
