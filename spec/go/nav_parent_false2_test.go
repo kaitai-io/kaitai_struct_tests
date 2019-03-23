27e3785686db0f655287042727f14ff62954a67e
Brad Erickson <bderickson@gmail.com>
2019-03-23 01:47:50 -0700

    Add Go test panic handling defer func()
    A panic() called in the tests breaks the test result processor, so
    Don't Panic and print a friendly stacktrace.

--- spec/go/nav_parent_false2_test.go
+++ spec/go/nav_parent_false2_test.go
@@ -1,6 +1,7 @@
 package spec
 
 import (
+	"runtime/debug"
 	"os"
 	"testing"
 
@@ -11,6 +12,12 @@ import (
 )
 
 func TestNavParentFalse2(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			debug.PrintStack()
+			t.Fatal("unexpected panic:", r)
+		}
+	}()
 	f, err := os.Open("../../src/fixed_struct.bin")
 	if err != nil {
 		t.Fatal(err)
