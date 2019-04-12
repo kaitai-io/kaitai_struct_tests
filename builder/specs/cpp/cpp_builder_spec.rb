874c8d05920b88d3fbe830e16530b72360b989e8
Mikhail Yakshin <greycat@altlinux.org>
2019-04-12 06:03:51 +0100

    More tests for CppBuilder (msbuild_1), better test infrastructure

--- builder/specs/cpp/cpp_builder_spec.rb
+++ builder/specs/cpp/cpp_builder_spec.rb
@@ -1,46 +1,67 @@
 require_relative '../../cpp_builder'
 
-$spec_dir = File.dirname(__FILE__)
-
 RSpec.describe CppBuilder do
+  before :context do
+    @spec_dir = File.dirname(__FILE__)
+  end
+
   context '1' do
-    Dir.chdir("#{$spec_dir}/1")
-    r = CppBuilder.new('compiled/cpp_stl_11', 'spec/cpp_stl_11', 'test_out/cpp_stl_11')
+    before :context do
+      Dir.chdir("#{@spec_dir}/1")
+      @builder = CppBuilder.new('compiled/cpp_stl_11', 'spec/cpp_stl_11', 'test_out/cpp_stl_11')
+    end
 
     describe '#list_mandatory_files' do
       it 'shows no mandatory files' do
-        expect(r.list_mandatory_files.to_a.sort).to eq []
+        expect(@builder.list_mandatory_files.to_a.sort).to eq []
       end
     end
 
     describe '#list_mandatory_files' do
       it 'shows no mandatory files' do
-        expect(r.list_mandatory_files.to_a.sort).to eq []
+        expect(@builder.list_mandatory_files.to_a.sort).to eq []
       end
     end
 
     describe '#parse_failed_build' do
       it 'parses failed build information for make' do
-        p Dir.pwd
-        expect(r.parse_failed_build('test_out/cpp_stl_11/build-1.log').to_a.sort).to eq ['/home/greycat/git/kaitai_struct/tests/compiled/cpp_stl_11/io_local_var.cpp']
+        expect(@builder.parse_failed_build('test_out/cpp_stl_11/build-1.log').to_a.sort).to eq ['/home/greycat/git/kaitai_struct/tests/compiled/cpp_stl_11/io_local_var.cpp']
       end
 
       it 'parses failed build information for ld' do
-        expect(r.parse_failed_build('test_out/cpp_stl_11/build-2.log').to_a.sort).to eq ['/home/greycat/git/kaitai_struct/tests/spec/cpp_stl_11/test_io_local_var.cpp']
+        expect(@builder.parse_failed_build('test_out/cpp_stl_11/build-2.log').to_a.sort).to eq ['/home/greycat/git/kaitai_struct/tests/spec/cpp_stl_11/test_io_local_var.cpp']
       end
     end
 
     describe '#file_to_test' do
       it 'parses spec filename' do
         expect(
-          r.file_to_test("/home/greycat/git/kaitai_struct/tests/spec/cpp_stl_11/test_io_local_var.cpp")
+          @builder.file_to_test("/home/greycat/git/kaitai_struct/tests/spec/cpp_stl_11/test_io_local_var.cpp")
         ).to eq [:spec, 'IoLocalVar']
       end
 
       it 'parses format filename' do
         expect(
-          r.file_to_test("/home/greycat/git/kaitai_struct/tests/compiled/cpp_stl_11/io_local_var.cpp")
+          @builder.file_to_test("/home/greycat/git/kaitai_struct/tests/compiled/cpp_stl_11/io_local_var.cpp")
         ).to eq [:format, 'IoLocalVar']
+      end
+    end
+  end
+
+  context 'msbuild_1' do
+    before :context do
+      Dir.chdir("#{@spec_dir}/msbuild_1")
+      @builder = CppBuilder.new('compiled/cpp_stl_98', 'spec/cpp_stl_98', 'test_out/cpp_stl_98')
+      @builder.mode = :msbuild_windows
+    end
+
+    describe '#parse_failed_build' do
+      it 'parses failed build information for msbuild' do
+        expect(@builder.parse_failed_build('test_out/cpp_stl_98/build-1.log').to_a.sort).to match_array [
+          "c:\\projects\\ci-targets\\tests\\compiled\\cpp_stl_98\\enum_to_i_class_border_2.cpp",
+          "c:\\projects\\ci-targets\\tests\\compiled\\cpp_stl_98\\enum_to_i_class_border_2.h",
+          "c:\\projects\\ci-targets\\tests\\compiled\\cpp_stl_98\\io_local_var.cpp",
+        ]
       end
     end
   end
--- builder/specs/csharp/csharp_builder_spec.rb
+++ builder/specs/csharp/csharp_builder_spec.rb
@@ -3,19 +3,25 @@ require_relative '../../csharp_builder'
 $spec_dir = File.dirname(__FILE__)
 
 RSpec.describe CSharpBuilder do
+  before :context do
+    @spec_dir = File.dirname(__FILE__)
+  end
+
   context '1' do
-    Dir.chdir("#{$spec_dir}/1")
-    r = CSharpBuilder.new
+    before :context do
+      Dir.chdir("#{@spec_dir}/1")
+      @builder = CSharpBuilder.new
+    end
 
     describe '#list_mandatory_files' do
       it 'shows no mandatory files' do
-        expect(r.list_mandatory_files.to_a.sort).to eq ["CommonSpec.cs", "Properties\\AssemblyInfo.cs"]
+        expect(@builder.list_mandatory_files.to_a.sort).to eq ["CommonSpec.cs", "Properties\\AssemblyInfo.cs"]
       end
     end
 
     describe '#parse_failed_build' do
       it 'parses failed build information' do
-        expect(r.parse_failed_build('test_out/csharp/build-1.log').to_a.sort).to eq(
+        expect(@builder.parse_failed_build('test_out/csharp/build-1.log').to_a.sort).to eq(
           ["\\home\\greycat\\git\\kaitai_struct\\tests\\spec\\csharp\\kaitai_struct_csharp_tests\\tests\\SpecProcessCustom.cs"]
         )
       end
@@ -24,16 +30,16 @@ RSpec.describe CSharpBuilder do
     describe '#file_to_test' do
       it 'parses spec filename' do
         expect(
-          r.file_to_test("\\home\\greycat\\git\\kaitai_struct\\tests\\spec\\csharp\\kaitai_struct_csharp_tests\\tests\\SpecProcessCustom.cs")
+          @builder.file_to_test("\\home\\greycat\\git\\kaitai_struct\\tests\\spec\\csharp\\kaitai_struct_csharp_tests\\tests\\SpecProcessCustom.cs")
         ).to eq [:spec, 'ProcessCustom']
         expect(
-          r.file_to_test("\\home\\greycat\\git\\kaitai_struct\\tests\\spec\\csharp\\kaitai_struct_csharp_tests\\tests\\SpecFooBar.cs")
+          @builder.file_to_test("\\home\\greycat\\git\\kaitai_struct\\tests\\spec\\csharp\\kaitai_struct_csharp_tests\\tests\\SpecFooBar.cs")
         ).to eq [:spec, 'FooBar']
       end
 
       it 'parses format filename' do
         expect(
-          r.file_to_test("\\home\\greycat\\git\\kaitai_struct\\tests\\compiled\\csharp\\NestedTypes.cs")
+          @builder.file_to_test("\\home\\greycat\\git\\kaitai_struct\\tests\\compiled\\csharp\\NestedTypes.cs")
         ).to eq [:format, 'NestedTypes']
       end
     end
