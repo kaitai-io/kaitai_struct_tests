6bcade31737639af243ee9670ab356e934a93a08
Mikhail Yakshin <greycat@altlinux.org>
2019-04-13 01:18:59 +0100

    Added java_builder, reworked run-java to use it

---
+++ builder/java_builder.rb
@@ -0,0 +1,99 @@
+require 'fileutils'
+require 'set'
+
+require_relative 'partial_builder'
+require_relative 'shellconfig'
+
+class JavaBuilder < PartialBuilder
+  def initialize
+    super
+
+    @spec_dir = 'spec/java/src'
+    @compiled_dir = "#{@config['FORMATS_COMPILED_DIR']}/java"
+    @project_file = "#{@compiled_dir}/src_list.txt"
+
+    @java_runtime_dir = @config['JAVA_RUNTIME_DIR'] or raise "JAVA_RUNTIME_DIR is undefined"
+    @java_testng_jar = @config['JAVA_TESTNG_JAR'] or raise "JAVA_TESTNG_JAR is undefined"
+
+    @java_classes_dir = "#{@compiled_dir}/bin"
+
+    @test_out_dir = "#{@config['TEST_OUT_DIR']}/java"
+  end
+
+  def list_mandatory_files
+    Dir.glob("#{@java_runtime_dir}/src/main/**/*.java")
+  end
+
+  def list_disposable_files
+    Dir.glob("#{@spec_dir}/**/*.java") + Dir.glob("#{@compiled_dir}/src/**/*.java")
+  end
+
+  def create_project(mand_files, disp_files)
+    File.open(@project_file, 'w') { |f|
+      (mand_files + disp_files).each { |l| f.puts l }
+    }
+    @project_file
+  end
+
+  def build_project(log_file)
+    FileUtils.mkdir_p(@java_classes_dir)
+    cli = [
+      'javac',
+      '-encoding', 'UTF-8',
+      '-cp', "#{@java_classes_dir}:#{@java_testng_jar}",
+      '-d', @java_classes_dir,
+      "@#{@project_file}",
+    ]
+    run_and_tee({}, cli, log_file).exitstatus
+  end
+
+  def parse_failed_build(log_file)
+    list = Set.new
+
+    File.open(log_file, 'r') { |f|
+      f.each_line { |l|
+        l.chomp!
+        if l =~ /^*(.+?\.java):(\d+): error: (.*?)$/
+          filename = $1
+          #row = $2
+          #msg = $3
+          list << filename
+        end
+      }
+    }
+
+    list
+  end
+
+  def file_to_test(filename)
+    # File.basename only forwards with forward slashes, so we normalize for that first
+    fn = File.basename(filename.gsub(/\\/, '/'))
+    if fn =~ /^Test(.*)\.java$/
+      return [:spec, $1]
+    else
+      return [:format, fn.gsub(/\.java$/, '')]
+    end
+  end
+
+  def run_tests
+    orig_dir = Dir.pwd
+    FileUtils.mkdir_p(@test_out_dir)
+    FileUtils.rm_rf("#{@test_out_dir}/junitreports")
+
+    cli = [
+      'java',
+      '-cp', "#{File.absolute_path(@java_classes_dir)}:#{File.absolute_path(@java_testng_jar)}",
+      'org.testng.TestNG',
+      '-d', "#{File.absolute_path(@test_out_dir)}", File.absolute_path('spec/java/testng.xml')
+    ]
+    out_log = "#{File.absolute_path(@test_out_dir)}/test_run.stdout"
+
+    # Java uses "../../src" to locate binary input files, so we change
+    # working directory prior to running tests to match that
+    Dir.chdir('spec/java')
+    run_and_tee({}, cli, out_log)
+    Dir.chdir(orig_dir)
+
+    File.exists?("#{@test_out_dir}/junitreports")
+  end
+end
