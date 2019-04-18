0e76446f1ac4792bcbb2981160d4cd362fce05f2
Mikhail Yakshin <greycat@altlinux.org>
2019-04-18 01:58:05 +0100

    Adjusted GraphViz parser to ignore non-specs

--- aggregate/text_list_parser.rb
+++ aggregate/text_list_parser.rb
@@ -1,5 +1,7 @@
 require_relative 'test_parser'
 
+require 'set'
+
 # Simplest test parser one can imagine: just a plain text file with a
 # list of tests that passed, one per line.
 class TextListParser < TestParser
@@ -7,9 +9,25 @@ class TextListParser < TestParser
     @doc = File.readlines(fn)
   end
 
+  # We don't have a list of *specs*, we just grab all the files we've
+  # ended up with. Some of them are parts of tests, and not the
+  # individual tests per se.
+  BLACK_LIST = Set.new([
+    'Imported1',
+    'Imported2',
+    'ImportedAndAbs',
+    'ImportedAndRel',
+    'ImportedRoot',
+    'ImportsCircularB',
+    'OpaqueExternalType02Child',
+    'ParamsPassStruct',
+    'VlqBase128Le',
+  ])
+
   def each_test
     @doc.each { |name|
       name = underscore_to_ucamelcase(name.chomp)
+      next if BLACK_LIST.include?(name)
       yield TestResult.new(name, :passed, 0, nil)
     }
   end
