172088e405b47fb502a57175335f8837999b883f
Bradlee Speice <bradlee@speice.io>
2019-05-13 11:32:50 -0400

    Commit the translator fixes

--- translator/src/main/scala/io/kaitai/struct/testtranslator/specgenerators/RustSG.scala
+++ translator/src/main/scala/io/kaitai/struct/testtranslator/specgenerators/RustSG.scala
@@ -1,66 +1,60 @@
 package io.kaitai.struct.testtranslator.specgenerators
 
-import _root_.io.kaitai.struct.{ClassTypeProvider, RuntimeConfig}
-import _root_.io.kaitai.struct.datatype.DataType
-import _root_.io.kaitai.struct.datatype.DataType._
-import _root_.io.kaitai.struct.exprlang.Ast
-import _root_.io.kaitai.struct.languages.RustCompiler
 import _root_.io.kaitai.struct.testtranslator.{Main, TestAssert, TestSpec}
-import _root_.io.kaitai.struct.translators.RustTranslator
+import io.kaitai.struct.datatype.DataType
+import io.kaitai.struct.exprlang.Ast
+import io.kaitai.struct.{ClassTypeProvider, RuntimeConfig}
+import io.kaitai.struct.languages.RustCompiler
+import io.kaitai.struct.translators.RustTranslator
 
-class RustSG(spec: TestSpec, provider: ClassTypeProvider) extends BaseGenerator(spec) {
-  val className = RustCompiler.type2class(spec.id)
+class RustSG(spec: TestSpec, provider: ClassTypeProvider)
+    extends BaseGenerator(spec) {
+  val className: String = RustCompiler.type2class(spec.id)
   val translator = new RustTranslator(provider, RuntimeConfig())
 
-  override def fileName(name: String): String = s"test_$name.rs"
+  override def fileName(name: String): String = s"tests/test_$name.rs"
 
   override def header(): Unit = {
-    out.puts("extern crate kaitai_struct;")
-    out.puts(s"extern crate rust;")
-    out.puts
-    
-    out.puts("use kaitai_struct::KaitaiStruct;")
-    out.puts(s"use rust::$className;")
+    out.puts("use kaitai::{BytesReader, KStruct, KStructUnit};")
+    out.puts(s"use kaitai_test::${spec.id}::$className;")
+    out.puts(s"use std::fs;")
     out.puts
 
     out.puts("#[test]")
     out.puts(s"fn test_${spec.id}() {")
     out.inc
 
-    out.puts("if let Ok(r) = " + className + "::from_file(\"src/" + spec.data + "\") {")
-    out.inc
+    out.puts("let data = fs::read(\"../../src/" + spec.data + "\").expect(\"Unable to read data.\");")
+    out.puts(s"let reader = BytesReader::new(&data[..]);")
+    out.puts(s"let r = $className::default().read(&reader, None, KStructUnit::parent_stack()).unwrap();")
   }
 
   override def footer(): Unit = {
     out.dec
     out.puts("}")
-    out.dec
-    out.puts("}")
+  }
+
+  override def nullAssert(actual: Ast.expr): Unit = {
+    val actStr = translateAct(actual)
+    out.puts(s"// nullAsset($actStr);")
+  }
+
+  def translateAct(x: Ast.expr): String =
+    translator.translate(x).replace("self." + Main.INIT_OBJ_NAME, "r")
+
+  override def trueArrayAssert(check: TestAssert,
+                               elType: DataType,
+                               elts: Seq[Ast.expr]): Unit = {
+    simpleAssert(check)
   }
 
   override def simpleAssert(check: TestAssert): Unit = {
     val actStr = translateAct(check.actual)
     val expStr = translator.translate(check.expected)
-    out.puts(s"assert_eq!($actStr, $expStr);")
-  }
-
-  override def nullAssert(actual: Ast.expr): Unit = {
-    val actStr = translateAct(actual)
-    out.puts(s"assertNull($actStr);")
-    // TODO: Figure out what's meant to happen here
-  }
-
-  override def trueArrayAssert(check: TestAssert, elType: DataType, elts: Seq[Ast.expr]): Unit = {
-    simpleAssert(check) // FIXME
+    out.puts(s"// assert_eq!($actStr, $expStr);")
   }
 
   override def indentStr: String = "    "
 
-  override def results: String = {
-    "// " + AUTOGEN_COMMENT + "\n\n" +
-      out.result
-  }
-
-  def translateAct(x: Ast.expr) =
-    translator.translate(x).replace("self." + Main.INIT_OBJ_NAME, "r")
+  override def results: String = "// " + AUTOGEN_COMMENT + "\n\n" + out.result
 }
