f3bb234f688211992bd72cbdfd28c9db61f48141
Martin Rodriguez <tinrodriguez8@gmail.com>
2019-05-31 12:38:16 -0300

    Added more seq's to scan-end test

--- formats/scan_custom.ksy
+++ formats/scan_custom.ksy
@@ -3,3 +3,7 @@ meta:
 seq:
   - id: s1
     scan-end: my_custom_scan(0x7c)
+  - id: s2
+    scan-end: my_custom_scan(0x7c)
+  - id: s3
+    scan-end: my_custom_scan(0x40)
--- spec/python/test_scan_custom.py
+++ spec/python/test_scan_custom.py
@@ -6,3 +6,5 @@ class TestMyCustomScan(unittest.TestCase):
     def test_scan_custom(self):
         with ScanCustom.from_file('src/term_strz.bin') as r:
             self.assertEqual(r.s1, b"\x66\x6F\x6F\x7c")
+            self.assertEqual(r.s2, b"\x62\x61\x72\x7c")
+            self.assertEqual(r.s3, b"\x62\x61\x7A\x40")
