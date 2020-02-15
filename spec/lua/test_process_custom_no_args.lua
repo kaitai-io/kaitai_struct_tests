1403bce4fa202f33759688a1a372dd2c1d3cd1ef
Petr Pucil <petr.pucil@seznam.cz>
2020-02-15 23:01:25 +0100

    Fix process_custom_no_args typo in Lua

--- spec/lua/test_process_custom_no_args.lua
+++ spec/lua/test_process_custom_no_args.lua
@@ -7,5 +7,5 @@ TestProcessCustomNoArgs = {}
 function TestProcessCustomNoArgs:test_process_custom_no_args()
     local r = ProcessCustomNoArgs:from_file("src/process_rotate.bin")
 
-    luaunit.assertEquals(r.buf1, "\x5F\x09\xAC\x8D\x8D\xED\x5F")
+    luaunit.assertEquals(r.buf, "\x5F\x09\xAC\x8D\x8D\xED\x5F")
 end
