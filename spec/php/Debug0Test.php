b4f31c64d6abf402ba10d4498468dce14feb63fa
cherue <cherue@noreply.users.github.com>
2020-03-07 18:08:02 +0100

    Reimplement markbook2@e38d2f348db98e7954692dd68d93aa8feb87bb06

--- spec/php/Debug0Test.php
+++ spec/php/Debug0Test.php
@@ -4,5 +4,6 @@ namespace Kaitai\Struct\Tests;
 class Debug0Test extends TestCase {
     public function testDebug0() {
         // no debug mode so far, nothing to test
+        $this->markTestSkipped();
     }
 }
