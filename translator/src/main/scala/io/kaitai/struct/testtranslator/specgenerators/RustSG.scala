f80f73c44e5212ca01f84d147ba46adf9e6d60ff
Jocelyn Kevorkian <jocelyn.kevorkian@thalesgroup.com>
2020-04-05 14:19:34 +0200

    Add import list to RustTranslator

--- translator/src/main/scala/io/kaitai/struct/testtranslator/specgenerators/RustSG.scala
+++ translator/src/main/scala/io/kaitai/struct/testtranslator/specgenerators/RustSG.scala
@@ -10,7 +10,8 @@ import _root_.io.kaitai.struct.translators.RustTranslator
 
 class RustSG(spec: TestSpec, provider: ClassTypeProvider) extends BaseGenerator(spec) {
   val className = RustCompiler.type2class(spec.id)
-  val translator = new RustTranslator(provider, RuntimeConfig())
+  val importList = new CppImportList
+  val translator = new RustTranslator(provider, importList, RuntimeConfig())
 
   override def fileName(name: String): String = s"test_$name.rs"
 
