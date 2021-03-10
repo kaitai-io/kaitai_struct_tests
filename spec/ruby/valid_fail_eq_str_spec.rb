706316fb159783ea3c599458d986eb9fc549b634
Thorsten Schöning <6223655+ams-tschoening@users.noreply.github.com>
2021-03-10 16:29:29 +0100

    Make sure that HEX is used for bytes.

--- spec/ruby/valid_fail_eq_str_spec.rb
+++ spec/ruby/valid_fail_eq_str_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ValidFailEqStr do
     expect {
       r = ValidFailEqStr.from_file('src/fixed_struct.bin')
     }.to raise_error(
-      # Make extra sure that handling of "byte[]" is not used by looking at the message.
+      # Make extra sure that handling of "byte[]" is NOT used by looking at the message.
       Kaitai::Struct::ValidationNotEqualError,
       a_string_including('"BACK"', '"PACK"')
     )
