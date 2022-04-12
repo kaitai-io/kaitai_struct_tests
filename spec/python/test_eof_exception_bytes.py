39ef93781019eed8b0152210a627f32493e30432
Petr Pucil <petr.pucil@seznam.cz>
2022-04-12 19:40:41 +0200

    Python: error msg must fully match the regex (not just contain)

--- spec/python/test_eof_exception_bytes.py
+++ spec/python/test_eof_exception_bytes.py
@@ -5,6 +5,6 @@ from eof_exception_bytes import EofExceptionBytes
 
 class TestEofExceptionBytes(unittest.TestCase):
     def test_eof_exception_bytes(self):
-        with self.assertRaisesRegexp(EOFError, "requested \d+ bytes, but only \d+ bytes available"):
+        with self.assertRaisesRegexp(EOFError, "^requested \d+ bytes, but only \d+ bytes available$"):
             with EofExceptionBytes.from_file('src/term_strz.bin') as r:
                 pass
--- spec/python/test_eof_exception_u4.py
+++ spec/python/test_eof_exception_u4.py
@@ -5,6 +5,6 @@ from eof_exception_u4 import EofExceptionU4
 
 class TestEofExceptionU4(unittest.TestCase):
     def test_eof_exception_u4(self):
-        with self.assertRaisesRegexp(EOFError, "requested \d+ bytes, but only \d+ bytes available"):
+        with self.assertRaisesRegexp(EOFError, "^requested \d+ bytes, but only \d+ bytes available$"):
             with EofExceptionU4.from_file('src/term_strz.bin') as r:
                 pass
