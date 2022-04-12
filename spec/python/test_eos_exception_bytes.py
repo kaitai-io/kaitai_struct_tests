32af2cd50765de3261cbbba7d33e04ccdd7effe2
Petr Pucil <petr.pucil@seznam.cz>
2022-04-12 19:23:08 +0200

    Python: use old name `assertRaisesRegexp` to support Python 2.7
    See https://docs.python.org/3/library/unittest.html#deprecated-aliases

--- spec/python/test_eos_exception_bytes.py
+++ spec/python/test_eos_exception_bytes.py
@@ -5,6 +5,6 @@ from eos_exception_bytes import EosExceptionBytes
 
 class TestEosExceptionBytes(unittest.TestCase):
     def test_eos_exception_bytes(self):
-        with self.assertRaisesRegex(EOFError, "requested \d+ bytes, but only \d+ bytes available"):
+        with self.assertRaisesRegexp(EOFError, "requested \d+ bytes, but only \d+ bytes available"):
             with EosExceptionBytes.from_file('src/term_strz.bin') as r:
                 pass
