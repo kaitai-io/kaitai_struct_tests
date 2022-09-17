0596437854c322b3bcfccc215e925a9966602ffa
Petr Pucil <petr.pucil@seznam.cz>
2022-09-17 19:36:47 +0200

    Add InstanceStd subtests for _check{Inst}() and _write{Inst}()
    See https://github.com/kaitai-io/kaitai_struct_compiler/commit/16374cc4508d662ba222095db3f75d03eb266164

--- spec/java/src/io/kaitai/struct/specwrite/TestInstanceStd.java
+++ spec/java/src/io/kaitai/struct/specwrite/TestInstanceStd.java
@@ -2,9 +2,14 @@
 
 package io.kaitai.struct.specwrite;
 
+import io.kaitai.struct.ByteBufferKaitaiStream;
+import io.kaitai.struct.ConsistencyError;
+import io.kaitai.struct.KaitaiStream;
 import io.kaitai.struct.KaitaiStruct.ReadWrite;
 import io.kaitai.struct.testwrite.InstanceStd;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+import java.lang.reflect.InvocationTargetException;
 
 public class TestInstanceStd extends CommonSpec {
     @Override
@@ -17,4 +22,56 @@ public class TestInstanceStd extends CommonSpec {
         return "str_encodings.bin";
     }
 
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: header,.*")
+    public void checkShorterHeader() throws Exception {
+        InstanceStd r = new InstanceStd();
+        r._check();
+        r.setHeader("1234");
+        r._checkHeader();
+    }
+
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: header,.*")
+    public void checkLongerHeader() throws Exception {
+        InstanceStd r = new InstanceStd();
+        r._check();
+        r.setHeader("123456");
+        r._checkHeader();
+    }
+
+    @Test(expectedExceptions = ConsistencyError.class, expectedExceptionsMessageRegExp = "Check failed: header,.*")
+    public void checkEmptyHeaderViaDump() throws Exception {
+        InstanceStd r = new InstanceStd();
+        r.setHeader("");
+        try {
+            // calls all _check*() methods
+            dumpStruct(r);
+        } catch (InvocationTargetException e) {
+            if (e.getCause() instanceof Exception) {
+                throw ((Exception) e.getCause());
+            }
+            throw e;
+        }
+    }
+
+    @Test
+    public void testWrite() throws Exception {
+        InstanceStd r = new InstanceStd();
+        r.setHeader("Hello");
+
+        // see <CommonSpec>.testReadWriteRoundtrip
+        Object origDump = dumpStruct(r);
+
+        KaitaiStream io = new ByteBufferKaitaiStream(2 + 5);
+        r._write(io);
+        io.alignToByte();
+        io.seek(0);
+        r._writeHeader();
+
+        InstanceStd newR = new InstanceStd(io);
+        newR._read();
+
+        Object newDump = dumpStruct(newR);
+
+        assertEquals(newDump, origDump);
+    }
 }
