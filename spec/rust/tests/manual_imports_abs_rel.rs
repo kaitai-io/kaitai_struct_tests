ed5599ccfb016a96ef9cc4d4b860fff1a72a6680
Oleg Dolgov <Oleg.Dolgov@gmail.com>
2022-09-24 12:51:07 +0200

    import tests.

---
+++ spec/rust/tests/manual_imports_abs_rel.rs
@@ -0,0 +1,22 @@
+use std::fs;
+
+extern crate kaitai;
+use self::kaitai::*;
+mod formats;
+use formats::imports_abs_rel::*;
+
+
+#[test]
+fn test_params_def() {
+    let bytes = fs::read("../../src/fixed_struct.bin").unwrap();
+    let reader = BytesReader::new(&bytes);
+    let mut r = ImportsAbsRel::default();
+
+    if let Err(err) = r.read(&reader, None, Some(KStructUnit::parent_stack())) {
+        panic!("{:?}", err);
+    }
+
+    assert_eq!(*r.one(), 80);
+    assert_eq!(*r.two().one(), 65);
+    assert_eq!(*r.two().two().one(), 67);
+}
