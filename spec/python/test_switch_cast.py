56575bcaad794e82680951f86e208250d8da244c
Petr Pucil <petr.pucil@seznam.cz>
2023-02-17 22:04:27 +0100

    Fix unclosed files in manual Python tests

--- spec/python/test_switch_cast.py
+++ spec/python/test_switch_cast.py
@@ -4,8 +4,10 @@ from switch_cast import SwitchCast
 
 class TestSwitchCast(unittest.TestCase):
     def test_switch_cast(self):
-        r = SwitchCast.from_file("src/switch_opcodes.bin")
+        with SwitchCast.from_file('src/switch_opcodes.bin') as r:
 
-        self.assertEqual(r.first_obj.value, "foobar")
-        self.assertEqual(r.second_val, 0x42)
-        # unable to test "err_cast" here
+            self.assertEqual(r.first_obj.value, "foobar")
+            self.assertEqual(r.second_val, 0x42)
+
+            self.assertNotIsInstance(r.err_cast, SwitchCast.Strval)
+            self.assertIsInstance(r.err_cast, SwitchCast.Intval)
