146846d4e364e6cf19e53e201c090446024ec13a
Petr Pucil <petr.pucil@seznam.cz>
2023-08-04 19:38:31 +0200

    Regen ExprToITrailing from KST for Java

--- spec/java/src/io/kaitai/struct/spec/TestExprToITrailing.java
+++ spec/java/src/io/kaitai/struct/spec/TestExprToITrailing.java
@@ -12,8 +12,18 @@ public class TestExprToITrailing extends CommonSpec {
     public void testExprToITrailing() throws Exception {
         ExprToITrailing r = ExprToITrailing.fromFile(SRC_DIR + "term_strz.bin");
 
-        assertThrows(NumberFormatException.class, () -> r.toIR10());
+        assertThrows(NumberFormatException.class, new ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                r.toIR10();
+            }
+        });
         assertIntEquals(r.toIR16(), 152517308);
-        assertThrows(NumberFormatException.class, () -> r.toIGarbage());
+        assertThrows(NumberFormatException.class, new ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                r.toIGarbage();
+            }
+        });
     }
 }
