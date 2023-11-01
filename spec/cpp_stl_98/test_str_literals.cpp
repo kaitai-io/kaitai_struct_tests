0c4f850a934d1eb80b21a6f45724976249bd46af
Petr Pucil <petr.pucil@seznam.cz>
2023-11-01 01:40:18 +0100

    Improve/add missing StrLiterals specs

--- spec/cpp_stl_98/test_str_literals.cpp
+++ spec/cpp_stl_98/test_str_literals.cpp
@@ -11,7 +11,7 @@ BOOST_AUTO_TEST_CASE(test_str_literals) {
     kaitai::kstream ks(&ifs);
     str_literals_t* r = new str_literals_t(&ks);
 
-    BOOST_CHECK_EQUAL(r->complex_str(), std::string("\x00\x01\x02\x07\x08\x0a\x0d\x09\x0b\x0c\x1b\x3d\x7\xa\x24\u263b", 18));
+    BOOST_CHECK_EQUAL(r->complex_str(), std::string("\x00\x01\x02\x07\x08\x0a\x0d\x09\x0b\x0c\x1b\x3d\x07\x0a\x24\u263b", 18));
     BOOST_CHECK_EQUAL(r->double_quotes(), "\x22\x22\x22");
     BOOST_CHECK_EQUAL(r->backslashes(), "\x5c\x5c\x5c");
     BOOST_CHECK_EQUAL(r->octal_eatup(), std::string("\x00\x32\x32", 3));
