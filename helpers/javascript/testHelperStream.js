a247c1f1fd6ab883678668c6adbe53108a01127f
Petr Pucil <petr.pucil@seznam.cz>
2024-02-16 14:59:03 +0100

    Fix testHelper{Stream,Throws}.js for non-existing parser
    This has been already done for helpers/javascript/testHelper.js (see
    321e29f227fd883ee5fc038235fc89ca522804e9).

--- helpers/javascript/testHelperStream.js
+++ helpers/javascript/testHelperStream.js
@@ -1,10 +1,10 @@
 function testHelperStream(className, fileName, testFunc) {
   var fs = require("fs");
   var KaitaiStream = global.KaitaiStream = require("kaitai-struct/KaitaiStream");
-  var parser = require(className);
 
   describe(className, function() {
     it('parses test properly', function(done) {
+      var parser = require(className);
       fs.readFile(fileName, function(err, buf) {
         var st = new KaitaiStream(buf);
         testFunc(st, parser);
--- helpers/javascript/testHelperThrows.js
+++ helpers/javascript/testHelperThrows.js
@@ -2,11 +2,11 @@ function testHelperThrows(className, fileName, excClass) {
   var fs = require("fs");
   var assert = require('assert');
   var KaitaiStream = require("kaitai-struct/KaitaiStream");
-  var parser = require(className);
 
   describe(className, function() {
     it('parses test properly', function(done) {
       assert.ok(excClass);
+      var parser = require(className);
       fs.readFile(fileName, function(err, buf) {
         var st = new KaitaiStream(buf);
         assert.throws(
