8226814412d2cbafd7ebe6ffd641941ea23458fe
Petr Pucil <petr.pucil@seznam.cz>
2024-03-18 22:17:38 +0100

    boost_test_parser: detect aborted tests
    The conditions for considering a test as aborted are purely empirical,
    so it may turn out in the future that there can be an aborted test for
    which some condition doesn't apply (at the moment I'm not aware of any
    such case).
    
    I just know that simply the presence of the `<Exception>` tag isn't
    enough to reliably detect an aborted test, because some `<Exception>`s
    are non-fatal (for example
    https://github.com/kaitai-io/ci_artifacts/blob/64438fbd11da816bcba374c23cd0e31c01fe4abe/test_out/cpp_stl_11/results.xml
    contains two `<TestCase>` tags with `<Exception>`, one `abort() has been
    called` is non-fatal and the other one `memory access violation occurred
    at address 0x00000010` is fatal).
    
    This is why we also check the presence of the `Test is aborted` message
    by `boost/test/impl/unit_test_log.ipp`, as it seems to always be
    printed from this source file exactly in the case of an aborted test
    (see https://www.boost.org/doc/libs/1_84_0/boost/test/impl/unit_test_log.ipp):
    
    ```cpp
    void
    unit_test_log_t::test_aborted()
    {
        BOOST_TEST_LOG_ENTRY( log_messages ) << "Test is aborted";
    }
    ```

--- aggregate/boost_test_parser.rb
+++ aggregate/boost_test_parser.rb
@@ -46,4 +46,42 @@ class BoostTestParser < TestParser
       }
     }
   end
+
+  def aborted_tests
+    tests = []
+    # An "aborted" test means that "a critical error is detected", which means
+    # that "the test execution is aborted and all remaining tests are discarded"
+    # (see https://www.boost.org/doc/libs/1_84_0/libs/test/doc/html/boost/unit_test/test_observer.html#idm46380-bb).
+    # So in practice there will probably never be more than 1 aborted test (per
+    # log file), but we don't rely on that here and return an array anyway.
+    each_aborted_test { |t| tests << t }
+    tests
+  end
+
+  def each_aborted_test
+    r = @doc.root
+    r.elements.each('TestSuite/TestCase') { |tc|
+      next if tc.elements['Exception'].nil?
+
+      aborted = false
+
+      tc.elements.each('Message') { |msg|
+        next unless msg.text == 'Test is aborted'
+
+        file_attr = msg.attribute('file')
+        next if file_attr.nil?
+
+        file = file_attr.value
+        next unless file.end_with?('/boost/test/impl/unit_test_log.ipp')
+
+        aborted = true
+        break
+      }
+
+      next unless aborted
+
+      name = tc.attribute('name').value
+      yield name
+    }
+  end
 end
