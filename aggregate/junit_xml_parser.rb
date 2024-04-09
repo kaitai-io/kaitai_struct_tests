436b49774672fe74262a74df26332ab5de96c035
Dias Rystin <drystin@azul.com>
2024-04-09 16:09:57 +0200

    add julia support for aggregator scripts

--- aggregate/junit_xml_parser.rb
+++ aggregate/junit_xml_parser.rb
@@ -33,6 +33,9 @@ class JUnitXMLParser < TestParser
         elsif name =~ /^t(?!est)(.*?)$/
           # Nim output
           name = underscore_to_ucamelcase($1)
+        elsif tc.attribute('classname') and tc.attribute('classname').value =~ /^\/(.*?) test$/
+          # Julia output
+          name = $1
         else
           raise "Unable to parse name: \"#{name}\"" unless name =~ /^[Tt]est(.*?)$/
           if $1[0] == '_'
@@ -70,6 +73,15 @@ class JUnitXMLParser < TestParser
           name = ts.attribute('name').value.gsub(/^.*\\/, '').gsub(/Test$/, '')
 
           tr = TestResult.new(name, :skipped, 0, nil)
+          yield tr
+        elsif ts.attribute('errors') && ts.attribute('errors').value.to_f != 0 && ts.attribute('name').value =~ /^\/(.*?) test$/
+          # Pick up Julia errored tests
+          name = $1
+          error_element = ts.elements['error']
+          error_message = error_element.attribute('message').value if error_element
+          error_trace = error_element.text.strip if error_element
+
+          tr = TestResult.new(name, :error, 0, TestResult::Failure.new(nil, nil, error_message, error_trace))
           yield tr
         end
       }
