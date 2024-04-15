c99c848df843799717ea7bb6ff0a55ea427abf26
Mingun <alexander_sergey@mail.ru>
2024-04-15 21:29:48 +0500

    Python: fix DeprecationWarning: invalid escape sequence '\d'
      with self.assertRaisesRegex(EOFError, "^requested \d+ bytes, but only \d+ bytes available$"):

--- spec/python/test_eof_exception_bytes.py
+++ spec/python/test_eof_exception_bytes.py
@@ -3,6 +3,6 @@ from eof_exception_bytes import EofExceptionBytes
 
 class TestEofExceptionBytes(BaseTest):
     def test_eof_exception_bytes(self):
-        with self.assertRaisesRegex(EOFError, "^requested \d+ bytes, but only \d+ bytes available$"):
+        with self.assertRaisesRegex(EOFError, r"^requested \d+ bytes, but only \d+ bytes available$"):
             with EofExceptionBytes.from_file('src/term_strz.bin') as r:
                 pass
--- spec/python/test_eos_exception_u4.py
+++ spec/python/test_eos_exception_u4.py
@@ -3,6 +3,6 @@ from eos_exception_u4 import EosExceptionU4
 
 class TestEosExceptionU4(BaseTest):
     def test_eos_exception_u4(self):
-        with self.assertRaisesRegex(EOFError, "^requested \d+ bytes, but only \d+ bytes available$"):
+        with self.assertRaisesRegex(EOFError, r"^requested \d+ bytes, but only \d+ bytes available$"):
             with EosExceptionU4.from_file('src/term_strz.bin') as r:
                 pass
