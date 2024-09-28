d97adee4efb25a7427fe563aa2665be235c964f4
Petr Pucil <petr.pucil@seznam.cz>
2024-09-29 01:59:42 +0200

    JavaScript: normalize byte array assertions in non-generated specs

--- spec/javascript/test_optional_id.js
+++ spec/javascript/test_optional_id.js
@@ -1,9 +1,8 @@
 var assert = require('assert');
 var testHelper = require('testHelper');
-var hexString = require('hexString');
 
 testHelper('OptionalId', 'src/fixed_struct.bin', function(r, OptionalId_) {
   assert.strictEqual(r._unnamed0, 80);
   assert.strictEqual(r._unnamed1, 65);
-  assert.strictEqual(hexString(r._unnamed2), hexString([67, 75, 45, 49, 255]));
+  assert.deepStrictEqual(r._unnamed2, new Uint8Array([67, 75, 45, 49, 255]));
 });
