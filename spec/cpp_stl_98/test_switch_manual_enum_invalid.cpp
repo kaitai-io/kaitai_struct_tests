dc6cc2abcd46eedcafa99bd63852bfac98ad9893
Petr Pucil <petr.pucil@seznam.cz>
2025-04-21 19:16:50 +0200

    SwitchManualEnumInvalid: assert that pointers are null in C++
    Related to https://github.com/kaitai-io/kaitai_struct/issues/244
    
    See similar change in the previous commit
    de597b3606afe59949c2f458f8d93bef05f95200

--- spec/cpp_stl_98/test_switch_manual_enum_invalid.cpp
+++ spec/cpp_stl_98/test_switch_manual_enum_invalid.cpp
@@ -14,8 +14,10 @@ BOOST_AUTO_TEST_CASE(test_switch_manual_enum_invalid) {
     BOOST_CHECK_EQUAL(r->opcodes()->size(), 2);
     BOOST_CHECK_EQUAL(r->opcodes()->at(0)->code(), static_cast<switch_manual_enum_invalid_t::opcode_t::code_enum_t>(255));
     BOOST_CHECK(r->opcodes()->at(0)->_is_null_body());
+    BOOST_CHECK(!r->opcodes()->at(0)->body()); // added manually
     BOOST_CHECK_EQUAL(r->opcodes()->at(1)->code(), static_cast<switch_manual_enum_invalid_t::opcode_t::code_enum_t>(1));
     BOOST_CHECK(r->opcodes()->at(1)->_is_null_body());
+    BOOST_CHECK(!r->opcodes()->at(1)->body()); // added manually
 
     delete r;
 }
