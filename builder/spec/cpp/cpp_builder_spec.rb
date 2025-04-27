36e95e7d5803a658b66e9dcb950f030a8d57af1f
Petr Pucil <petr.pucil@seznam.cz>
2025-04-27 21:47:32 +0200

    boost_test_parser: move tests from `builder/` to `aggregate/`

--- builder/spec/cpp/cpp_builder_spec.rb
+++ builder/spec/cpp/cpp_builder_spec.rb
@@ -1,5 +1,4 @@
 require_relative '../../cpp_builder'
-require_relative '../../../aggregate/boost_test_parser'
 require 'rspec' # normally not needed, but RubyMine doesn't autocomplete RSpec methods without it
 
 RSpec.describe CppBuilder do
@@ -398,40 +397,6 @@ RSpec.describe CppBuilder do
       it 'parses failed build information' do
         expect(@builder.parse_failed_build('test_out/cpp_stl_98/build-2.log')).to eq [
           'c:/projects/ci-targets/tests/spec/cpp_stl_98/test_expr_calc_array_ops.cpp',
-        ]
-      end
-    end
-  end
-
-  # The log comes from https://github.com/kaitai-io/ci_artifacts/blob/8b27b4eeaa8653cd33940fb620c184cd9763e6f0/test_out/cpp_stl_11/results.xml
-  context 'boost_aborted_gcc' do
-    before :context do
-      Dir.chdir("#{@spec_dir}/boost_aborted_gcc")
-    end
-
-    describe 'BoostTestParser#aborted_tests' do
-      it 'parses aborted tests' do
-        infile = 'test_out/cpp_stl_11'
-        p = BoostTestParser.new("#{infile}/results.xml")
-        expect(p.aborted_tests).to eq [
-          'test_debug_array_user',
-        ]
-      end
-    end
-  end
-
-  # The log comes from https://github.com/kaitai-io/ci_artifacts/blob/a588faa1a35b658116d4ab5551f0863e61a7324c/test_out/cpp_stl_11/results.xml
-  context 'boost_aborted_msvc' do
-    before :context do
-      Dir.chdir("#{@spec_dir}/boost_aborted_msvc")
-    end
-
-    describe 'BoostTestParser#aborted_tests' do
-      it 'parses aborted tests' do
-        infile = 'test_out/cpp_stl_11'
-        p = BoostTestParser.new("#{infile}/results.xml")
-        expect(p.aborted_tests).to eq [
-          'test_debug_array_user',
         ]
       end
     end
