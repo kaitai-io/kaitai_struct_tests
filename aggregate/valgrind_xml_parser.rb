cb7cdc8b37f63e32fff790a7676b1064eb1d8b67
Petr Pucil <petr.pucil@seznam.cz>
2025-04-29 23:15:56 +0200

    valgrind_xml_parser: support errors other than leaks, add tests
    Also detect errors that occurred only in the test spec (i.e. in a
    `spec/cpp_stl_*/test_*.cpp` file), not in the generated parser
    (`compiled/cpp_stl_*/*.cpp`).

--- aggregate/valgrind_xml_parser.rb
+++ aggregate/valgrind_xml_parser.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 require_relative 'test_parser'
 
 require 'rexml/document'
 require 'set'
 
-def underscore_to_ucamelcase(s)
-  s.split(/_/).map { |x| x.capitalize }.join
-end
-
+# See the documentation of Valgrind's XML output at
+# https://sourceware.org/git/?p=valgrind.git;a=blob;f=docs/internals/xml-output-protocol4.txt;hb=0b557127300197e0c779369d2e173eb85121fd66
 class ValgrindXMLParser < TestParser
   def initialize(fn)
     @doc = REXML::Document.new(File.read(fn))
@@ -18,28 +18,58 @@ class ValgrindXMLParser < TestParser
 
       err.elements.each('stack') { |stack|
         stack.elements.each('frame') { |frame|
-          dir1 = frame.elements['dir']
-          dir = dir1.nil? ? nil : dir1.text
-          if dir =~ /\/compiled\/cpp_stl/
-            file = frame.elements['file'].text
-            test_name = file.gsub(/\.cpp$/, '')
-            affected << test_name
-          end
+          path = frame_to_path(frame)
+          next unless path
+
+          kind = file_to_kind(path)
+          next unless kind
+
+          basename = File.basename(path, '.cpp')
+          test_name = kind == :spec ? basename.delete_prefix('test_') : basename
+          affected << test_name
         }
       }
 
-      unless affected.empty?
-        msg = if err.elements['xwhat']
-                err.elements['xwhat'].elements['text'].text
-              else
-                nil
-              end
-        failure = TestResult::Failure.new(nil, nil, msg, nil)
-        affected.each { |name|
-          tr = TestResult.new(underscore_to_ucamelcase(name), :leak, nil, failure)
-          yield tr
-        }
-      end
+      next if affected.empty?
+
+      err_kind = err.elements['kind'].text
+      status =
+        if err_kind.start_with?('Leak_')
+          :leak
+        else
+          :failed
+        end
+      msg =
+        if err.elements['xwhat']
+          err.elements['xwhat'].elements['text'].text
+        elsif err.elements['what']
+          err.elements['what'].text
+        end
+      failure = TestResult::Failure.new(nil, nil, msg, nil)
+      affected.each { |name|
+        tr = TestResult.new(underscore_to_ucamelcase(name), status, nil, failure)
+        yield tr
+      }
     }
+  end
+
+  private
+
+  def frame_to_path(frame)
+    dir = frame.elements['dir']
+    return nil unless dir
+
+    file = frame.elements['file']
+    return nil unless file
+
+    File.join(dir.text, file.text)
+  end
+
+  def file_to_kind(path)
+    if path.include?('/compiled/cpp_stl')
+      :format
+    elsif path.include?('/spec/cpp_stl')
+      :spec
+    end
   end
 end
