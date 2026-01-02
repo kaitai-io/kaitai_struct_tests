8d24324796e409225e1c27bb6a59d49b12089633
Petr Pucil <petr.pucil@seznam.cz>
2026-01-02 11:31:03 +0100

    Update comments on why we use empty `__init__.py` files
    We no longer do this for compatibility with Python 2, but for a
    different reason.

--- spec/python/extra/nested/__init__.py
+++ spec/python/extra/nested/__init__.py
@@ -1,1 +1,3 @@
-# This is required only for Python 2.7 compatibility. Python 3 does not need empty __init__.py.
+# Although Python 3 doesn't require empty `__init__.py` files, omitting them will create
+# a [namespace package](https://docs.python.org/3/glossary.html#term-namespace-package).
+# This is a relatively advanced feature and is recommended to be avoided unless needed.
