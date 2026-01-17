e4df22fb21b5663383c7fe04a060ceec4e913245
Petr Pucil <petr.pucil@seznam.cz>
2026-01-17 16:36:52 +0100

    Construct: remove blank line in manual specs for consistency

--- spec/construct/test_str_literals.py
+++ spec/construct/test_str_literals.py
@@ -5,7 +5,6 @@ from str_literals import _schema
 class TestStrLiterals(unittest.TestCase):
     def test_str_literals(self):
         r = _schema.parse_file('src/fixed_struct.bin')
-
         self.assertEqual(self.str_to_arr(r.complex_str), [0, 1, 2, 7, 8, 10, 13, 9, 11, 12, 27, 61, 7, 10, 36, 9787])
         self.assertEqual(self.str_to_arr(r.double_quotes), [34, 34, 34])
         self.assertEqual(self.str_to_arr(r.backslashes), [92, 92, 92])
