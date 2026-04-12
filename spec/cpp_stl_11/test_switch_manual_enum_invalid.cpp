105fb76cbc79f4c91e815becc5b7359c67dc8c3e
Mingun <Alexander_Sergey@mail.ru>
2026-04-12 19:44:06 +0500

    C++: Regenerate tests after generating nullptr checks in the previous commit
    Tests regenerated using this command and manually cleaned up from unnecessary changes:
    ./spec_kst_to_all -t cpp_stl_98 -t cpp_stl_11 --all-specs -f

--- spec/cpp_stl_11/test_switch_manual_enum_invalid.cpp
+++ spec/cpp_stl_11/test_switch_manual_enum_invalid.cpp
@@ -14,10 +14,10 @@ BOOST_AUTO_TEST_CASE(test_switch_manual_enum_invalid) {
     BOOST_CHECK_EQUAL(r->opcodes()->size(), 2);
     BOOST_CHECK_EQUAL(r->opcodes()->at(0)->code(), 255);
     BOOST_CHECK(r->opcodes()->at(0)->_is_null_body());
-    BOOST_CHECK_EQUAL(r->opcodes()->at(0)->body(), nullptr); // added manually
+    BOOST_CHECK_EQUAL(r->opcodes()->at(0)->body(), nullptr);
     BOOST_CHECK_EQUAL(r->opcodes()->at(1)->code(), 1);
     BOOST_CHECK(r->opcodes()->at(1)->_is_null_body());
-    BOOST_CHECK_EQUAL(r->opcodes()->at(1)->body(), nullptr); // added manually
+    BOOST_CHECK_EQUAL(r->opcodes()->at(1)->body(), nullptr);
 
     delete r;
 }
